de58fccfc4b23f446c739c471f9e7eb0f6d98735
Levi Bostian <levi@customer.io>
2021-08-13 11:09:40 -0500

    chore: help determine undocumented code

--- hooks/post-install.sh
+++ hooks/post-install.sh
@@ -8,9 +8,10 @@ RED='\033[0;31m'
 echo -e "${RED}Hooks installed! Now, make sure that you have these tools installed on your machine:"
 
 echo -e "${YELLOW}You need to install the program 'swiftformat' on your machine to continue."
-echo ""
 echo -e "${YELLOW}The easiest way is 'brew install swiftformat'. If you're not on macOS, check out other instructions for installing: https://github.com/nicklockwood/SwiftFormat#command-line-tool"
 echo ""
 echo -e "${YELLOW}You need to install the program 'swiftlint' on your machine to continue."
-echo ""
 echo -e "${YELLOW}The easiest way is 'brew install swiftlint'. If you're not on macOS, check out other instructions for installing: https://github.com/realm/SwiftLint#installation"
+echo ""
+echo -e "${YELLOW}If you would like to see what code you have not yet written reference docs for, uou need to install the program 'jazzy' on your machine to continue."
+echo -e "${YELLOW}The easiest way is 'gem install jazzy'. Else, check out other instructions for installing: https://github.com/realm/jazzy/#installation"
