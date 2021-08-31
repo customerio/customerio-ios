f4e51312bcad7b47d58f1dbb7fd8fb6d5c410d3e
Stephen Young <stephen@customer.io>
2021-08-31 16:45:32 -0600

    APN delegate

--- Sources/MessagingPushAPN/MessagingPushAPN.swift
+++ Sources/MessagingPushAPN/MessagingPushAPN.swift
@@ -1,16 +1,23 @@
 import Foundation
+import UIKit
 import CioTracking
 import CioMessagingPush
 
 /**
  Swift code goes into this module that is specific to APN push notification messaging.
   */
-public class MessagingPushAPN: MessagingPush {
-    public func register(deviceToken: Data, onComplete: @escaping (Result<Void, CustomerIOError>) -> Void) {
-        self.registerDeviceToken(deviceToken: deviceToken, onComplete: onComplete)
+public extension MessagingPush {
+    
+    func registerForRemoteNotifications(){
+        UIApplication.shared.registerForRemoteNotifications()
     }
     
-    public func unregister(onComplete: @escaping (Result<Void, CustomerIOError>) -> Void) {
-        self.deleteDeviceToken(onComplete: onComplete)
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data, onComplete: @escaping (Result<Void, CustomerIOError>) -> Void) {
+        self.registerDeviceToken(deviceToken: deviceToken, onComplete:onComplete)
+      }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        // Print to XCode logs the error
     }
 }
+
