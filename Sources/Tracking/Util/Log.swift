cc8733bfcfcf25cff99eff1e2fad0e3459db80b2
Levi Bostian <levi@customer.io>
2021-12-03 14:07:14 -0600

    fix ci building project

--- Sources/Tracking/Util/Log.swift
+++ Sources/Tracking/Util/Log.swift
@@ -2,9 +2,6 @@ import Foundation
 #if canImport(os)
 import os.log
 #endif
-#if canImport(OSLog)
-import OSLog
-#endif
 
 /// mockable logger + abstract that allows you to log to multiple places if you wish
 public protocol Logger: AutoMockable {
@@ -27,17 +24,17 @@ public class ConsoleLogger: Logger {
     private let logSubsystem = "io.customer.sdk"
     private let logCategory = "CIO"
 
+    #if canImport(os)
     // Unified logging for Swift. https://www.avanderlee.com/workflow/oslog-unified-logging/
     // This means we can view logs in xcode console + Console app.
     private func printMessage(_ message: String, _ level: OSLogType) {
-        #if canImport(os)
-        newOsLog(message, level)
-        #elseif canImport(OSLog)
-        oldOsLog(message, level)
-        #endif
-
-        // At this time, Linux cannot use `os.log` or `OSLog`. Instead, use: https://github.com/apple/swift-log/
-        // As we don't officially support Linux at this time, no need to add a dependency to the project.
+        if #available(iOS 14, *) {
+            let logger = os.Logger(subsystem: self.logSubsystem, category: self.logCategory)
+            logger.info("\(message, privacy: .public)")
+        } else {
+            let logger = OSLog(subsystem: logSubsystem, category: logCategory)
+            os_log("%{public}@", log: logger, type: .info, message)
+        }
     }
 
     public func debug(_ message: String) {
@@ -51,24 +48,12 @@ public class ConsoleLogger: Logger {
     public func error(_ message: String) {
         printMessage(message, .error)
     }
-}
-
-extension ConsoleLogger {
-    private func newOsLog(_ message: String, _ level: OSLogType) {
-        #if canImport(os)
-        if #available(iOS 14, *) {
-            let logger = os.Logger(subsystem: self.logSubsystem, category: self.logCategory)
-            logger.info("\(message, privacy: .public)")
-        } else {
-            oldOsLog(message, level)
-        }
-        #endif
-    }
-
-    private func oldOsLog(_ message: String, _ level: OSLogType) {
-        #if canImport(OSLog)
-        let logger = OSLog(subsystem: logSubsystem, category: logCategory)
-        os_log("%{public}@", log: logger, type: .info, message)
-        #endif
-    }
+    #else
+    // At this time, Linux cannot use `os.log` or `OSLog`. Instead, use: https://github.com/apple/swift-log/
+    // As we don't officially support Linux at this time, no need to add a dependency to the project.
+    // therefore, we are not logging if can't import os.log
+    public func debug(_ message: String) {}
+    public func info(_ message: String) {}
+    public func error(_ message: String) {}
+    #endif
 }
