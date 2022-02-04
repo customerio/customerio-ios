0878f5d1beda1dcaf454a5ec89dccd6a5bcaa8ed
Levi Bostian <levi@customer.io>
2022-02-04 09:38:54 -0600

    add danger to help us catch breaking changes in PRs

---
+++ dangerfile.js
@@ -0,0 +1,18 @@
+// File that Danger runs to catch potential errors during PR reviews: https://danger.systems/js/
+import {message, danger, warn} from "danger"
+
+// Warn about possible breaking changes being introduced in a pull request. 
+// Breaking changes should be documented when making a pull request. This is a reminder. 
+let modifiedPublicApiFiles = danger.git.modified_files.filter((filePath) => filePath.endsWith("APITest.swift"))
+modifiedPublicApiFiles.forEach((filePath) => {
+  warn(`
+I noticed file `${filePath}` was modified. That could mean that this pull request is introducing a *breaking change* to the SDK. 
+  
+If this pull request *does* introduce a breaking change, make sure the pull request title is in the format: 
+\`\`\` 
+<type>!: description of breaking change 
+// Example:
+refactor!: remove onComplete callback from async functions 
+\`\`\`
+`)
+})
