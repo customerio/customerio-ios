7048c33ecf042a22c75a01f91bb83f01e227331f
Levi Bostian <levi@customer.io>
2022-02-07 09:04:55 -0600

    add mocks to tests

--- Sources/MessagingPushFCM/autogenerated/AutoLenses.generated.swift
+++ Sources/MessagingPushFCM/autogenerated/AutoLenses.generated.swift
@@ -2,39 +2,39 @@
 // DO NOT EDIT
 // swiftlint:disable all
 
+import Foundation
 import CioMessagingPush
 import CioTracking
-import Foundation
 
 /**
- ######################################################
- Documentation
- ######################################################
+######################################################
+Documentation
+######################################################
 
- This automatically generated file you are viewing is to modify immutable objects in a convenient way.
+This automatically generated file you are viewing is to modify immutable objects in a convenient way.
 
- * What do you mean modify immutable objects? https://apiumhub.com/tech-blog-barcelona/lenses-swift-immutability-objects/
+* What do you mean modify immutable objects? https://apiumhub.com/tech-blog-barcelona/lenses-swift-immutability-objects/
 
- * How do I use this?
+* How do I use this?
 
- ```
- // Add `AutoLenses` protocol to struct.
- struct Foo: AutoLenses {
-   // properties be `let` to be immutable
-   let bar: String
-   let bar2: Bool
- }
+```
+// Add `AutoLenses` protocol to struct.
+struct Foo: AutoLenses {
+  // properties be `let` to be immutable
+  let bar: String
+  let bar2: Bool
+}
 
- var foo = Foo(bar: "X", bar2: true)
- // Now, we want to modify `foo.bar` but it's immutable. How do I modify `bar` without having to do something messy like...
- Foo(bar: "new value", bar2: oldInstance.bar2)
- ...to leave my other properties alone?
+var foo = Foo(bar: "X", bar2: true)
+// Now, we want to modify `foo.bar` but it's immutable. How do I modify `bar` without having to do something messy like...
+Foo(bar: "new value", bar2: oldInstance.bar2)
+...to leave my other properties alone?
 
- foo = foo.setBar("new value")
- // Now, `foo` is set to the brand new instanced of `Foo` while copying over all other values of `Foo` from the old instance.
- ```
+foo = foo.setBar("new value")
+// Now, `foo` is set to the brand new instanced of `Foo` while copying over all other values of `Foo` from the old instance.
+```
 
- */
+*/
 
 infix operator *~: MultiplicationPrecedence
 infix operator |>: AdditionPrecedence
@@ -44,19 +44,22 @@ struct Lens<Whole, Part> {
     let set: (Part, Whole) -> Whole
 }
 
-func * <A, B, C>(lhs: Lens<A, B>, rhs: Lens<B, C>) -> Lens<A, C> {
-    Lens<A, C>(get: { a in rhs.get(lhs.get(a)) },
-               set: { c, a in lhs.set(rhs.set(c, lhs.get(a)), a) })
+func * <A, B, C> (lhs: Lens<A, B>, rhs: Lens<B, C>) -> Lens<A, C> {
+    return Lens<A, C>(
+        get: { a in rhs.get(lhs.get(a)) },
+        set: { (c, a) in lhs.set(rhs.set(c, lhs.get(a)), a) }
+    )
 }
 
-func *~ <A, B>(lhs: Lens<A, B>, rhs: B) -> (A) -> A {
-    { a in lhs.set(rhs, a) }
+func *~ <A, B> (lhs: Lens<A, B>, rhs: B) -> (A) -> A {
+    return { a in lhs.set(rhs, a) }
 }
 
-func |> <A, B>(x: A, f: (A) -> B) -> B {
-    f(x)
+func |> <A, B> (x: A, f: (A) -> B) -> B {
+    return f(x)
 }
 
-func |> <A, B, C>(f: @escaping (A) -> B, g: @escaping (B) -> C) -> (A) -> C {
-    { g(f($0)) }
+func |> <A, B, C> (f: @escaping (A) -> B, g: @escaping (B) -> C) -> (A) -> C {
+    return { g(f($0)) }
 }
+
--- Tests/Tracking/APITest.swift
+++ Tests/Tracking/APITest.swift
@@ -1,4 +1,4 @@
-@testable import CioTracking
+import CioTracking // do not use `@testable` so we can test functions are made public and not `internal`.
 import Foundation
 import SharedTests
 import XCTest
@@ -17,7 +17,12 @@ class TrackingAPITest: UnitTest {
     }
 
     let encodableData = EncodableExample(foo: "")
+    
+    // Test that public functions are accessible by mocked instances
+    let mock = CustomerIOInstanceMock()
 
+    // This function checks that public functions exist for the SDK and they are callable.
+    // Maybe we forgot to add a function? Maybe we forgot to make a function `public`?
     func test_allPublicTrackingFunctions() {
         _ = XCTSkip()
 
@@ -30,25 +35,50 @@ class TrackingAPITest: UnitTest {
 
         // Identify
         CustomerIO.shared.identify(identifier: "")
+        mock.identify(identifier: "")
         // TO FIX: we don't offer a [String: Any] version of identify()
         // CustomerIO.shared.identify(identifier: "", body: dictionaryData)
+        // mock.identify(identifier: "", body: dictionaryData)
         CustomerIO.shared.identify(identifier: "", body: encodableData)
+        mock.identify(identifier: "", body: encodableData)
 
         // clear identify
         CustomerIO.shared.clearIdentify()
+        mock.clearIdentify()
 
         // event tracking
         CustomerIO.shared.track(name: "")
+        mock.track(name: "")
         // TO FIX: we don't offer a [String: Any] version of track()
         // CustomerIO.shared.track(name: "", data: dictionaryData)
+        // mock.track(name: "", data: dictionaryData)
         CustomerIO.shared.track(name: "", data: encodableData)
+        mock.track(name: "", data: encodableData)
 
         // screen tracking
         CustomerIO.shared.screen(name: "")
+        mock.screen(name: "")
         CustomerIO.shared.screen(name: "", data: dictionaryData)
+        mock.screen(name: "", data: dictionaryData)
         CustomerIO.shared.screen(name: "", data: encodableData)
+        mock.screen(name: "", data: encodableData)
 
         // profile attributes
         CustomerIO.shared.profileAttributes = dictionaryData
+        mock.profileAttributes = dictionaryData
+    }
+    
+    func test_allPublicSdkConfigOptions() {
+        _ = XCTSkip()
+        
+        CustomerIO.config {
+            $0.trackingApiUrl = ""
+            $0.autoTrackPushEvents = true
+            $0.backgroundQueueMinNumberOfTasks = 10
+            $0.backgroundQueueSecondsDelay = 10
+            $0.logLevel = .error
+            $0.autoTrackPushEvents = false
+            $0.autoScreenViewBody = {return [:]}
+        }
     }
 }
