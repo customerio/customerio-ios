76c218609b55b84d2fd5996f0451e5eb9ba870a6
Levi Bostian <levi@customer.io>
2022-02-07 08:21:38 -0600

    fix spelling error

--- Tests/MessagingPushFCM/APITest.swift
+++ Tests/MessagingPushFCM/APITest.swift
@@ -9,7 +9,7 @@ import XCTest
  Contains an example of every public facing SDK function call. This file helps
  us prevent introducing breaking changes to the SDK by accident. If a public function
  of the SDK is modified, this test class will not successfully compile. By not compiling,
- that is a reminder to either fix the comilation and introduce the breaking change or
+ that is a reminder to either fix the compilation and introduce the breaking change or
  fix the mistake and not introduce the breaking change in the code base.
  */
 class MessagingPushFCMAPITest: UnitTest {
