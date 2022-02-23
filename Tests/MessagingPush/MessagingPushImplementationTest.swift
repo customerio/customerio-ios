55f33e736b2292764b52ea828373b53058fee7f0
ami-aman <91549653+ami-aman@users.noreply.github.com>
2022-02-23 19:33:09 +0530

    Update MessagingPushImplementationTest.swift

--- Tests/MessagingPush/MessagingPushImplementationTest.swift
+++ Tests/MessagingPush/MessagingPushImplementationTest.swift
@@ -40,8 +40,8 @@ class MessagingPushImplementationTest: UnitTest {
         profileStoreMock.identifier = givenIdentifier
         queueMock.addTaskReturnValue = (success: true, queueStatus: QueueStatus.successAddingSingleTask)
 
-//        messagingPush.registerDeviceToken(givenDeviceToken)
-        messagingPush.addDeviceAttributes(deviceToken: givenDeviceToken)
+        messagingPush.registerDeviceToken(givenDeviceToken)
+
         XCTAssertEqual(queueMock.addTaskCallsCount, 1)
         XCTAssertEqual(queueMock.addTaskReceivedArguments?.type, QueueTaskType.registerPushToken.rawValue)
         let actualQueueTaskData = queueMock.addTaskReceivedArguments!.data
