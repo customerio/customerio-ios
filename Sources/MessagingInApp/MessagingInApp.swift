52da106f6214f0677ab68832b08bc69ba0391b7d
Levi Bostian <levi@customer.io>
2022-05-27 15:45:35 -0500

    create api tests for in-app module

--- Sources/MessagingInApp/MessagingInApp.swift
+++ Sources/MessagingInApp/MessagingInApp.swift
@@ -3,7 +3,7 @@ import Common
 import Foundation
 import Gist
 
-public protocol MessagingInAppInstance {
+public protocol MessagingInAppInstance: AutoMockable {
     func initialize(organizationId: String)
 }
 
