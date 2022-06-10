5e9890ccb2d95e20b9b6b6c5a557579b450de05e
Levi Bostian <levi@customer.io>
2022-06-10 08:00:52 -0500

    add docs [skip ci]

--- Sources/Common/Util/Mocks.swift
+++ Sources/Common/Util/Mocks.swift
@@ -1,15 +1,22 @@
 import Foundation
 
+// Mock classes are generated automatically in this project.
+// To make using mocks easier in our test classes, this class exists to manage all of the mocks in the project.
+//
+// This class is only used for tests but must exist in `Common` module because mock classes are generated in the source code
+// directory and not the test code directory of the project.
 public class Mocks {
     public static var shared: Mocks = .init()
 
     private var mocks: [Mock] = []
     private init() {}
 
+    // This gets called automatically by the automatically generated mock classes code.
     public func add(mock: Mock) {
         mocks.append(mock)
     }
 
+    // Call this function in test teardown to reset the state of all mocks in your test.
     public func resetAll() {
         mocks.forEach {
             $0.resetMock()
@@ -17,6 +24,7 @@ public class Mocks {
     }
 }
 
+// All automatically generated mocks classes inherit this protocol automatically.
 public protocol Mock {
     func resetMock()
 }
