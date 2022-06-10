f206b030afd47bd4f9b7a7f6be9f37d335cc3b60
Levi Bostian <levi@customer.io>
2022-06-10 10:26:24 -0500

    fix: send attributes in all caps to API

--- Sources/Tracking/CustomerIOImplementation.swift
+++ Sources/Tracking/CustomerIOImplementation.swift
@@ -80,7 +80,7 @@ public class CustomerIOImplementation: CustomerIOInstance {
             guard let existingProfileIdentifier = profileStore.identifier else {
                 return
             }
-            identify(identifier: existingProfileIdentifier, body: StringAnyEncodable(newValue))
+            identify(identifier: existingProfileIdentifier, body: newValue)
         }
     }
 
@@ -117,7 +117,8 @@ public class CustomerIOImplementation: CustomerIOInstance {
             }
         }
 
-        let jsonBodyString = jsonAdapter.toJsonString(body, encoder: nil)
+        // Custom attributes so do not modify keys in JSON string
+        let jsonBodyString = jsonAdapter.toJsonString(body, convertKeysToSnakecase: false)
         logger.debug("identify profile attributes \(jsonBodyString ?? "none")")
 
         let queueTaskData = IdentifyProfileQueueTaskData(identifier: identifier,
@@ -217,7 +218,7 @@ extension CustomerIOImplementation {
         let data: AnyEncodable = (data == nil) ? AnyEncodable(EmptyRequestBody()) : AnyEncodable(data)
 
         let requestBody = TrackRequestBody(type: type, name: name, data: data, timestamp: Date())
-        guard let jsonBodyString = jsonAdapter.toJsonString(requestBody, encoder: nil) else {
+        guard let jsonBodyString = jsonAdapter.toJsonString(requestBody) else {
             logger.error("attributes provided for \(eventTypeDescription) \(name) failed to JSON encode.")
             return
         }
