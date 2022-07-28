ae6270473a95e046881d880a24525e4ca9ae32e8
ami-aman <91549653+ami-aman@users.noreply.github.com>
2022-07-28 19:36:23 +0400

    adding public init

--- Sources/Common/Type/SDKWrapperConfig.swift
+++ Sources/Common/Type/SDKWrapperConfig.swift
@@ -14,4 +14,9 @@ public struct SdkWrapperConfig {
     public enum Source: String {
         case reactNative = "ReactNative"
     }
+    
+    public init(source: Source, version: String) {
+        self.source = source
+        self.version = version
+    }
 }
