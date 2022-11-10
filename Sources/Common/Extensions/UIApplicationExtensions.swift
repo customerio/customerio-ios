8f30bab25898c2a0d05cd0588d4ae880f1456c83
Levi Bostian <levi@customer.io>
2022-11-10 13:00:58 -0600

    add docs explaining my code [skip ci]

--- Sources/Common/Extensions/UIApplicationExtensions.swift
+++ Sources/Common/Extensions/UIApplicationExtensions.swift
@@ -3,7 +3,7 @@ import Foundation
 import UIKit
 
 public extension UIApplication {
-    @available(iOSApplicationExtension, unavailable)
+    @available(iOSApplicationExtension, unavailable) // since some extension functions may be able to run in iOS app extensions, only disable for this 1 function
     func open(url: URL) {
         open(url, options: [:]) { _ in }
     }
