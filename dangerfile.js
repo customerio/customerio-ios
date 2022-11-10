71be462a9f2495eeb1585775882e02c16f1782e5
Levi Bostian <levi@customer.io>
2022-11-10 11:50:02 -0600

    ci: dont forget to update SPM or cocoapods

--- dangerfile.js
+++ dangerfile.js
@@ -18,3 +18,13 @@ refactor!: remove onComplete callback from async functions
 
   warn(message)
 })
+
+// The SDK is deployed to multiple dependency management softwares (Cocoapods and Swift Package Manager). 
+// This code tries to prevent forgetting to update metadata files for one but not the other. 
+let isSPMFilesModified = danger.git.modified_files.contains('Package.swift') || danger.git.modified_files.contains('Package.resolved')
+let isCococapodsFilesModified = danger.git.modified_files.filter((filePath) => filePath.endsWith('.podspec'))
+
+if (isSPMFilesModified || isCococapodsFilesModified) {
+  if (!isSPMFilesModified) { warn("Cocoapods files (*.podspec) were modified but Swift Package Manager files (Package.*) files were not. This is error-prone when updating dependencies in one service but not the other. Double-check that you updated all of the correct files.") }
+  if (!isCococapodsFilesModified) { warn("Swift Package Manager files (Package.*) were modified but Cocoapods files (*.podspec) files were not. This is error-prone when updating dependencies in one service but not the other. Double-check that you updated all of the correct files.") }
+}
