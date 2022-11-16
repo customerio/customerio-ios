5ef489a2153740006ce78cec9a6c310bd2235e52
Levi Bostian <levi@customer.io>
2022-11-15 23:14:35 -0600

    deliveryid can be null for in-app message

--- Sources/MessagingInApp/Type/InAppMessage.swift
+++ Sources/MessagingInApp/Type/InAppMessage.swift
@@ -6,13 +6,12 @@ typealias GistMessage = Message
 public struct InAppMessage: Equatable {
     public let instanceId: String
     public let messageId: String
-    public let deliveryId: String // (Currently taken from Gist's campaignId property)
+    public let deliveryId: String? // (Currently taken from Gist's campaignId property). Can be nil when sending test
+    // in-app messages
 
     internal init(gistMessage: GistMessage) {
         self.instanceId = gistMessage.instanceId
         self.messageId = gistMessage.messageId
-        // The Gist SDK source code always populates the campaign-id. Having a nil campaignId is only common using the
-        // `public init()` for Message.
-        self.deliveryId = gistMessage.gistProperties.campaignId!
+        self.deliveryId = gistMessage.gistProperties.campaignId
     }
 }
