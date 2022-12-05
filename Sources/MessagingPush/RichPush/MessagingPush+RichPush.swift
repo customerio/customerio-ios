96fd33510aae4a085941148b45242f05768880f6
Levi Bostian <levi@customer.io>
2022-12-05 14:48:25 -0600

    fix: simple pushes now processing correctly

--- Sources/MessagingPush/RichPush/MessagingPush+RichPush.swift
+++ Sources/MessagingPush/RichPush/MessagingPush+RichPush.swift
@@ -48,55 +48,67 @@ extension MessagingPushImplementation {
         _ request: UNNotificationRequest,
         withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void
     ) -> Bool {
-        logger.info("did recieve notification request. Checking if message was a rich push sent from Customer.io...")
+        logger.info("did recieve notification request. Checking if message was a push sent from Customer.io...")
         logger.debug("notification request: \(request.content.userInfo)")
 
-        if sdkConfig.autoTrackPushEvents,
-           let deliveryID: String = request.content.userInfo["CIO-Delivery-ID"] as? String,
-           let deviceToken: String = request.content.userInfo["CIO-Delivery-Token"] as? String {
+        guard let deliveryID: String = request.content.userInfo["CIO-Delivery-ID"] as? String,
+              let deviceToken: String = request.content.userInfo["CIO-Delivery-Token"] as? String
+        else {
+            logger.info("the notification was not sent by Customer.io. Ignoring notification request.")
+            return false
+        }
+
+        logger.info("push was sent from Customer.io. Processing the request...")
+
+        if sdkConfig.autoTrackPushEvents {
             logger.info("automatically tracking push metric: delivered")
             logger.debug("parsed deliveryId \(deliveryID), deviceToken: \(deviceToken)")
 
             trackMetric(deliveryID: deliveryID, event: .delivered, deviceToken: deviceToken)
         }
 
-        guard let pushContent = CustomerIOParsedPushPayload.parse(
+        if let richPushContent = CustomerIOParsedPushPayload.parse(
             notificationContent: request.content,
             jsonAdapter: jsonAdapter
-        )
-        else {
-            // push does not contain a CIO rich payload, so end early
-            logger.info("the notification was not sent by Customer.io. Ignoring notification request.")
-            return false
-        }
+        ) {
+            logger
+                .info("""
+                Parsing notification request to display rich content such as images, deep links, etc.
+                """)
+            logger.debug("push content: \(richPushContent)")
 
-        logger
-            .info("""
-            the notification was sent by Customer.io.
-            Parsing notification request to display rich content such as images, deep links, etc.
-            """)
-        logger.debug("push content: \(pushContent)")
+            RichPushRequestHandler.shared.startRequest(
+                request,
+                content: richPushContent,
+                siteId: siteId
+            ) { notificationContent in
+                self.logger.debug("rich push was composed \(notificationContent).")
 
-        RichPushRequestHandler.shared.startRequest(
-            request,
-            content: pushContent,
-            siteId: siteId
-        ) { notificationContent in
-            self.logger.debug("rich push was composed \(notificationContent).")
-
-            self.logger
-                .debug(
-                    "running all background queue tasks and waiting until complete to prevent OS from killing notification service extension before all HTTP requests have been performed"
-                )
-            self.backgroundQueue.run {
-                self.logger.debug("all background queue tasks done running.")
-                self.logger.info("Customer.io rich push processing is done!")
-
-                contentHandler(notificationContent)
+                self.finishTasksThenReturn(contentHandler: contentHandler, notificationContent: notificationContent)
             }
+        } else {
+            logger.info("the push was a simple push, not a rich push. Processing is complete.")
+
+            finishTasksThenReturn(contentHandler: contentHandler, notificationContent: request.content)
         }
 
         return true
+    }
+
+    private func finishTasksThenReturn(
+        contentHandler: @escaping (UNNotificationContent) -> Void,
+        notificationContent: UNNotificationContent
+    ) {
+        logger
+            .debug(
+                "running all background queue tasks and waiting until complete to prevent OS from killing notification service extension before all HTTP requests have been performed"
+            )
+        backgroundQueue.run {
+            self.logger.debug("all background queue tasks done running.")
+            self.logger.info("Customer.io push processing is done!")
+
+            contentHandler(notificationContent)
+        }
     }
 
     /**
