877efd96f48f41010de0c1b9add9e67a15f0122f
Levi Bostian <levi@customer.io>
2023-01-06 08:41:46 -0600

    tests: reproduce 1.2.8 bug in v2 to see if exists

--- Tests/MessagingInApp/MessagingInAppIntegrationTest.swift
+++ Tests/MessagingInApp/MessagingInAppIntegrationTest.swift
@@ -1,21 +1,20 @@
-@testable import CioTracking
 @testable import CioMessagingInApp
+@testable import CioTracking
 @testable import Common
 import Foundation
 import SharedTests
 import XCTest
 
 class MessagingInAppIntegrationTests: IntegrationTest {
-    
-    // Reproduce bug: https://github.com/customerio/customerio-ios/issues/242
+    // Bug reported when: using in-app messaging, debug logging enabled, and using iOS version 1.2.8.
+    // Report: https://github.com/customerio/customerio-ios/issues/242
     func test_initialize_enableDebugLogs_() {
-        CustomerIO.resetSharedInstance()
-        
-        CustomerIO.initialize(siteId: testSiteId, apiKey: .random)
-        CustomerIO.config {
+        tearDown() // re-initialize with different configuration options.
+
+        CustomerIO.initialize(siteId: testSiteId, apiKey: .random, region: .EU) {
             $0.logLevel = .debug
         }
-        
-        MessagingInApp.shared.initialize(organizationId: .random)
+
+        MessagingInApp.initialize(organizationId: .random) // would crash because DiGraph not initialized
     }
 }
