fa29c7854b71af287df817e2f0190fac74b7b197
Levi Bostian <levi@customer.io>
2023-02-15 09:59:08 -0600

    add docs

--- Sources/MessagingPush/Util/DeepLinkUtil.swift
+++ Sources/MessagingPush/Util/DeepLinkUtil.swift
@@ -19,17 +19,29 @@ class DeepLinkUtilImpl: DeepLinkUtil {
     }
 
     func handleDeepLink(_ deepLinkUrl: URL) {
-        logger.debug("Found a deep link inside of a push notification \(deepLinkUrl)")
+        logger.info("Found a deep link inside of a push notification.")
+        logger.debug("deep link found in push: \(deepLinkUrl)")
 
-        // First, try to open the link inside of the host app. This is to keep compatability with Universal Links.
-        // Learn more of edge case: https://github.com/customerio/customerio-ios/issues/262
-        // Fallback to opening the URL system-wide if fail to open link in host app.
-        // Customers with Universal Links in their app will need to add this function to their `AppDelegate` which will get called with deep link:
-        // func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool
+        /*
+         There are 2 types of deep links:
+         1. Universal Links which give URL format of a webpage using `http://` or `https://`
+         2. App scheme which give URL format using a prototol other then `http://` or `https://`.
+
+         First, try to open the link inside of the host app. This is to keep compatability with Universal Links.
+         Learn more of edge case: https://github.com/customerio/customerio-ios/issues/262
+
+         Fallback to opening the URL through a sytem call if:
+         1. deep link is an app scheme URL
+         2. Customer has not implemented the correct function in their host app to handle universal link:
+         ```
+         func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool
+         ```
+         3. Customer returned `false` from ^^^ function.
+         */
         let ifHandled = uiKit.continueNSUserActivity(webpageURL: deepLinkUrl)
 
         if !ifHandled {
-            logger.debug("Opening deep link through system call.")
+            logger.debug("Opening deep link through system call. Deep link: \(deepLinkUrl)")
             uiKit.open(url: deepLinkUrl)
         }
     }
