ddb8c4bb7e21c77709202b7e497b44d4b29b26f4
Levi Bostian <levi@customer.io>
2023-02-15 09:53:54 -0600

    code review changes

--- Sources/MessagingPush/MessagingPush+AppDelegate.swift
+++ Sources/MessagingPush/MessagingPush+AppDelegate.swift
@@ -1,11 +1,10 @@
 import Common
 import Foundation
-#if canImport(UserNotifications) && canImport(UIKit)
-import UIKit
+#if canImport(UserNotifications)
 import UserNotifications
 #endif
 
-#if canImport(UserNotifications) && canImport(UIKit)
+#if canImport(UserNotifications)
 @available(iOSApplicationExtension, unavailable)
 public extension MessagingPush {
     /**
@@ -64,35 +63,8 @@ extension MessagingPushImplementation {
 
         switch response.actionIdentifier {
         case UNNotificationDefaultActionIdentifier: // push notification was touched.
-            if let deepLinkurl = pushContent.deepLink {
-                // First, try to open the link inside of the host app. This is to keep compatability with Universal Links.
-                // Learn more of edge case: https://github.com/customerio/customerio-ios/issues/262
-                // Fallback to opening the URL system-wide if fail to open link in host app.
-                // Customers with Universal Links in their app will need to add this function to their `AppDelegate` which will get called with deep link:
-                // func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool
-
-                // fallback to open link in device browser
-                let performDeepLinkFallback: () -> Void = {
-                    self.logger.debug("Host app didn't handle link. Opening the link through a system call.")
-                    UIApplication.shared.open(url: deepLinkurl)
-                }
-
-                logger.debug("Found a deep link inside of a push notification. \(deepLinkurl)")
-
-                if deepLinkUtil.isLinkValidNSUserActivityLink(deepLinkurl) {
-                    logger.debug("Attempting to open deep link in host app.")
-
-                    let openLinkInHostAppActivity = NSUserActivity(activityType: NSUserActivityTypeBrowsingWeb)
-                    openLinkInHostAppActivity.webpageURL = deepLinkurl
-
-                    let didHostAppHandleLink = UIApplication.shared.delegate?.application?(UIApplication.shared, continue: openLinkInHostAppActivity, restorationHandler: { _ in }) ?? false
-
-                    if !didHostAppHandleLink {
-                        performDeepLinkFallback()
-                    }
-                } else {
-                    performDeepLinkFallback()
-                }
+            if let deepLinkUrl = pushContent.deepLink {
+                deepLinkUtil.handleDeepLink(deepLinkUrl)
             }
         default: break
         }
--- Sources/MessagingPush/Util/DeepLinkUtil.swift
+++ Sources/MessagingPush/Util/DeepLinkUtil.swift
@@ -1,21 +1,36 @@
 import Common
 import Foundation
+#if canImport(UIKit)
+import UIKit
+#endif
 
 protocol DeepLinkUtil: AutoMockable {
-    func isLinkValidNSUserActivityLink(_ url: URL) -> Bool
+    func handleDeepLink(_ deepLinkUrl: URL)
 }
 
 // sourcery: InjectRegister = "DeepLinkUtil"
 class DeepLinkUtilImpl: DeepLinkUtil {
-    // When using `NSUserActivity.webpageURL`, only certain URL schemes are allowed. An exception will be thrown otherwise which is why we have this function.
-    func isLinkValidNSUserActivityLink(_ url: URL) -> Bool {
-        guard let schemeOfUrl = url.scheme else {
-            return false
+    private let logger: Logger
+    private let uiKit: UIKitWrapper
+
+    init(logger: Logger, uiKitWrapper: UIKitWrapper) {
+        self.logger = logger
+        self.uiKit = uiKitWrapper
+    }
+
+    func handleDeepLink(_ deepLinkUrl: URL) {
+        logger.debug("Found a deep link inside of a push notification \(deepLinkUrl)")
+
+        // First, try to open the link inside of the host app. This is to keep compatability with Universal Links.
+        // Learn more of edge case: https://github.com/customerio/customerio-ios/issues/262
+        // Fallback to opening the URL system-wide if fail to open link in host app.
+        // Customers with Universal Links in their app will need to add this function to their `AppDelegate` which will get called with deep link:
+        // func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool
+        let ifHandled = uiKit.continueNSUserActivity(webpageURL: deepLinkUrl)
+
+        if !ifHandled {
+            logger.debug("Opening deep link through system call.")
+            uiKit.open(url: deepLinkUrl)
         }
-
-        // All allowed schemes in docs: https://developer.apple.com/documentation/foundation/nsuseractivity/1418086-webpageurl
-        let allowedSchemes = ["http", "https"]
-
-        return allowedSchemes.contains(schemeOfUrl)
     }
 }
