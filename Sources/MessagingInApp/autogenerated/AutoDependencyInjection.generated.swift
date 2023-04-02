bf1a26ac396de46ff54ca3dd3dea36e35b165854
Shahroz Khan <shrozali@gmail.com>
2023-04-03 03:38:00 +0500

    sourcery

--- Sources/MessagingInApp/autogenerated/AutoDependencyInjection.generated.swift
+++ Sources/MessagingInApp/autogenerated/AutoDependencyInjection.generated.swift
@@ -2,89 +2,90 @@
 // DO NOT EDIT
 // swiftlint:disable all
 
-import Foundation
 import CioTracking
 import Common
+import Foundation
 import Gist
 
 /**
-######################################################
-Documentation
-######################################################
+ ######################################################
+ Documentation
+ ######################################################
 
-This automatically generated file you are viewing is a dependency injection graph for your app's source code.
-You may be wondering a couple of questions. 
+ This automatically generated file you are viewing is a dependency injection graph for your app's source code.
+ You may be wondering a couple of questions.
 
-1. How did this file get generated? Answer --> https://github.com/levibostian/Sourcery-DI#how
-2. Why use this dependency injection graph instead of X other solution/tool? Answer --> https://github.com/levibostian/Sourcery-DI#why-use-this-project
-3. How do I add dependencies to this graph file? Follow one of the instructions below:
-* Add a non singleton class: https://github.com/levibostian/Sourcery-DI#add-a-non-singleton-class
-* Add a generic class: https://github.com/levibostian/Sourcery-DI#add-a-generic-class
-* Add a singleton class: https://github.com/levibostian/Sourcery-DI#add-a-singleton-class
-* Add a class from a 3rd party library/SDK: https://github.com/levibostian/Sourcery-DI#add-a-class-from-a-3rd-party
-* Add a `typealias` https://github.com/levibostian/Sourcery-DI#add-a-typealias
+ 1. How did this file get generated? Answer --> https://github.com/levibostian/Sourcery-DI#how
+ 2. Why use this dependency injection graph instead of X other solution/tool? Answer --> https://github.com/levibostian/Sourcery-DI#why-use-this-project
+ 3. How do I add dependencies to this graph file? Follow one of the instructions below:
+ * Add a non singleton class: https://github.com/levibostian/Sourcery-DI#add-a-non-singleton-class
+ * Add a generic class: https://github.com/levibostian/Sourcery-DI#add-a-generic-class
+ * Add a singleton class: https://github.com/levibostian/Sourcery-DI#add-a-singleton-class
+ * Add a class from a 3rd party library/SDK: https://github.com/levibostian/Sourcery-DI#add-a-class-from-a-3rd-party
+ * Add a `typealias` https://github.com/levibostian/Sourcery-DI#add-a-typealias
 
-4. How do I get dependencies from the graph in my code? 
-```
-// If you have a class like this:
-class OffRoadWheels {}
+ 4. How do I get dependencies from the graph in my code?
+ ```
+ // If you have a class like this:
+ class OffRoadWheels {}
 
-class ViewController: UIViewController {
-    // Call the property getter to get your dependency from the graph: 
-    let wheels = DIGraph.getInstance(siteId: "").offRoadWheels
-    // note the name of the property is name of the class with the first letter lowercase. 
-}
-```
+ class ViewController: UIViewController {
+     // Call the property getter to get your dependency from the graph:
+     let wheels = DIGraph.getInstance(siteId: "").offRoadWheels
+     // note the name of the property is name of the class with the first letter lowercase.
+ }
+ ```
 
-5. How do I use this graph in my test suite? 
-```
-let mockOffRoadWheels = // make a mock of OffRoadWheels class 
-DIGraph().override(mockOffRoadWheels, OffRoadWheels.self) 
-```
+ 5. How do I use this graph in my test suite?
+ ```
+ let mockOffRoadWheels = // make a mock of OffRoadWheels class
+ DIGraph().override(mockOffRoadWheels, OffRoadWheels.self)
+ ```
 
-Then, when your test function finishes, reset the graph:
-```
-DIGraph().reset()
-```
+ Then, when your test function finishes, reset the graph:
+ ```
+ DIGraph().reset()
+ ```
 
-*/
-
-
+ */
 
 extension DIGraph {
-    // call in automated test suite to confirm that all dependnecies able to resolve and not cause runtime exceptions. 
-    // internal scope so each module can provide their own version of the function with the same name. 
-    @available(iOSApplicationExtension, unavailable) // some properties could be unavailable to app extensions so this function must also. 
-    internal func testDependenciesAbleToResolve() -> Int {
+    // call in automated test suite to confirm that all dependnecies able to resolve and not cause runtime exceptions.
+    // internal scope so each module can provide their own version of the function with the same name.
+    @available(iOSApplicationExtension, unavailable) // some properties could be unavailable to app extensions so this function must also.
+    func testDependenciesAbleToResolve() -> Int {
         var countDependenciesResolved = 0
 
-        _ = self.inAppProvider
+        _ = inAppProvider
         countDependenciesResolved += 1
 
-        _ = self.moduleHookProvider
+        _ = moduleHookProvider
         countDependenciesResolved += 1
 
-        return countDependenciesResolved    
+        return countDependenciesResolved
     }
 
     // InAppProvider
-    internal var inAppProvider: InAppProvider {  
-        if let overridenDep = self.overrides[String(describing: InAppProvider.self)] {
+    var inAppProvider: InAppProvider {
+        if let overridenDep = overrides[String(describing: InAppProvider.self)] {
             return overridenDep as! InAppProvider
         }
-        return self.newInAppProvider
+        return newInAppProvider
     }
-    private var newInAppProvider: InAppProvider {    
-        return GistInAppProvider()
+
+    private var newInAppProvider: InAppProvider {
+        GistInAppProvider()
     }
+
     // ModuleHookProvider
-    internal var moduleHookProvider: ModuleHookProvider {  
-        if let overridenDep = self.overrides[String(describing: ModuleHookProvider.self)] {
+    var moduleHookProvider: ModuleHookProvider {
+        if let overridenDep = overrides[String(describing: ModuleHookProvider.self)] {
             return overridenDep as! ModuleHookProvider
         }
-        return self.newModuleHookProvider
+        return newModuleHookProvider
     }
-    private var newModuleHookProvider: ModuleHookProvider {    
-        return MessagingInAppModuleHookProvider()
+
+    private var newModuleHookProvider: ModuleHookProvider {
+        MessagingInAppModuleHookProvider()
     }
 }
