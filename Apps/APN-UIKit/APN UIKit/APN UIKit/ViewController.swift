f8a63ae1ed3f92b1ebd269f7423e3ea8dc76a0f3
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-04-10 18:55:43 +0400

    setting up structure with UIScene (UIKit)

---
+++ Apps/APN-UIKit/APN UIKit/APN UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APN UIKit
+//
+//  Created by Amandeep Kaur on 10/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
