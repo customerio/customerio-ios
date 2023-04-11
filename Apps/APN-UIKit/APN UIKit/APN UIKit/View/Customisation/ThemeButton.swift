ca1952fe3b534e8e8d217294ff627998cb08256a
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-04-11 15:16:32 +0400

    adding theme button with corner radius

--- Apps/APN-UIKit/APN UIKit/APN UIKit/View/Customisation/ThemeButton.swift
+++ Apps/APN-UIKit/APN UIKit/APN UIKit/View/Customisation/ThemeButton.swift
@@ -2,12 +2,19 @@ import UIKit
 
 class ThemeButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
     }
-    */
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        self.layer.cornerRadius = 5
+        self.frame.size.height = 50
+    }
 
 }
