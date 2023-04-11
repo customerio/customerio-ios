406792d0ec106234efba748ca8656113075d9694
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-04-11 16:35:52 +0400

    login validation

--- Apps/APN-UIKit/APN UIKit/APN UIKit/View/LoginViewController.swift
+++ Apps/APN-UIKit/APN UIKit/APN UIKit/View/LoginViewController.swift
@@ -2,21 +2,37 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: - Outlets
+    
+    @IBOutlet weak var emailTextField: ThemeTextField!
+    @IBOutlet weak var firstNameTextField: ThemeTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logInToApp(_ sender: UIButton) {
+        
+        if !userDetailsValid() {
+            return
+        }
+        
+        
     }
-    */
-
+    
+    @IBAction func generateRandomCredentials(_ sender: UIButton) {
+        let letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890"
+        let name = (String((0..<15).map{ _ in letters.randomElement()! }))
+        let email  = "\(name)@customer.io"
+        
+        // Set values
+        emailTextField.text = email
+        firstNameTextField.text = name
+    }
+    
+    func userDetailsValid() -> Bool {
+    
+        return !firstNameTextField.isTextTrimEmpty && !emailTextField.isTextTrimEmpty
+    }
 }
