e25ef41bcd2fedb4b9c96023d281897b3acdcc91
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-04-12 21:36:54 +0400

    customize screen based on source

--- Apps/APN-UIKit/APN UIKit/APN UIKit/View/Customisation/CustomDataViewController.swift
+++ Apps/APN-UIKit/APN UIKit/APN UIKit/View/Customisation/CustomDataViewController.swift
@@ -1,11 +1,22 @@
 import UIKit
 
+enum CustomDataSource {
+    case customEvents
+    case deviceAttributes
+    case profileAttributes
+}
+
 class CustomDataViewController: UIViewController {
 
     @IBOutlet weak var eventNameTextField: ThemeTextField!
     @IBOutlet weak var propertyValueTextField: ThemeTextField!
     @IBOutlet weak var propertyNameTextField: ThemeTextField!
+    @IBOutlet weak var headerLabel: UILabel!
     
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var propertyValueLabel: UILabel!
+    @IBOutlet weak var propertyNameLabel: UILabel!
+    var source : CustomDataSource = .customEvents
     static func newInstance() -> CustomDataViewController {
         UIStoryboard.getViewController(identifier: "CustomDataViewController")
     }
@@ -13,6 +24,7 @@ class CustomDataViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
          super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = false
+        customizeScreenBasedOnSource()
     }
     
     override func viewDidLoad() {
@@ -21,7 +33,36 @@ class CustomDataViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func customizeScreenBasedOnSource() {
+        
+        if source == .customEvents {
+            headerLabel.text = "Send Custom Event"
+        }
+        else {
+            headerLabel.text = source == .deviceAttributes ? "Set Custom Device Attribute" : "Set Custom Profile Atrribute"
+            eventNameLabel.isHidden = true
+            eventNameTextField.isHidden = true
+            propertyNameLabel.text = "Attribute Name"
+            propertyValueLabel.text = "Attribute Value"
+        }
+    }
+    
+    func customizeFor(attributeType : CustomDataSource){
+        if attributeType == .deviceAttributes {
+            
+        }
+        eventNameLabel.isHidden = true
+        eventNameTextField.isHidden = true
+        propertyNameLabel.text = "Attribute Name"
+        propertyValueLabel.text = "Attribute Value"
+    }
     // MARK: - Actions
     
-
+    @IBAction func sendCustomData(_ sender: UIButton) {
+        
+        if ( source == .customEvents) {
+            self.showInfoAlert(withMessage: "Name = \(eventNameTextField.text ?? "") and property name  = \(propertyNameTextField.text ?? "") and property value = \(propertyValueTextField.text ?? "")")
+        }
+    }
+    
 }
