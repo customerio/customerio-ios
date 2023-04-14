46d583b892586f86e57ec540d270faaf36e28c77
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-04-14 23:55:12 +0400

    push notifications integration

--- Apps/APN-UIKit/APN UIKit/APN UIKit/AppDelegate.swift
+++ Apps/APN-UIKit/APN UIKit/APN UIKit/AppDelegate.swift
@@ -1,12 +1,11 @@
 import UIKit
 import CioTracking
 import CioMessagingInApp
+import CioMessagingPushAPN
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -14,14 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // didRegisterForRemoteNotifications delegate method will be called and it
         // provides a device token. In case, registration fails then
         // didFailToRegisterForRemoteNotifications will be called.
+        
         application.registerForRemoteNotifications()
-        
         initializeCioAndInAppListeners()
-        
         UNUserNotificationCenter.current().delegate = self
         return true
     }
-
+    
     func initializeCioAndInAppListeners() {
         // Initialise CustomerIO SDK
         // TODO: - Update this when using local storage for configurations
@@ -33,27 +31,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         MessagingInApp.initialize(eventListener: self)
     }
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        
+        MessagingPush.shared.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
     }
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        
+        MessagingPush.shared.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
     }
-
 }
 
 
