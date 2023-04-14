b19e8d41843f0faa8633222ef14e9fa06367ae78
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-04-14 21:56:33 +0400

    adding outlets

--- Apps/APN-UIKit/APN UIKit/APN UIKit/View/SettingsViewController.swift
+++ Apps/APN-UIKit/APN UIKit/APN UIKit/View/SettingsViewController.swift
@@ -1,10 +1,21 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
     static func newInstance() -> SettingsViewController {
         UIStoryboard.getViewController(identifier: "SettingsViewController")
     }
+    
+    // MARK: - Outlets
+    @IBOutlet weak var deviceTokenTextField: ThemeTextField!
+    @IBOutlet weak var apiKeyTextField: ThemeTextField!
+    @IBOutlet weak var siteIdTextField: ThemeTextField!
+    @IBOutlet weak var trackUrlTextField: ThemeTextField!
+    @IBOutlet weak var trackDeviceToggle: UISwitch!
+    @IBOutlet weak var debugModeToggle: UISwitch!
+    @IBOutlet weak var trackScreenToggle: UISwitch!
+    @IBOutlet weak var enablePushToggle: UISwitch!
+    @IBOutlet weak var bgQMinTasks: ThemeTextField!
+    @IBOutlet weak var bgQTakDelayTextField: ThemeTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
