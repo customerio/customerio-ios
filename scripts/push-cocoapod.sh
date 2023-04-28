0946a76624568b26f27ad75889c692562ab43707
Levi Bostian <levi@customer.io>
2023-04-28 13:49:52 -0500

    ci: fix manually pushing cocoapods and re-running failures

--- scripts/push-cocoapod.sh
+++ scripts/push-cocoapod.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e # fail script if an error is encountered 
+
 # Pushes a given Cocoapod to the Cocoapods server for deployment. 
 # Use script: ./scripts/push-cocoapod.sh CustomerIO.podspec
 
@@ -27,5 +29,8 @@ echo "If you do notice an error message when trying to push a pod,"
 echo "1. Check this github repo https://github.com/search?q=repo%3ACocoaPods%2FSpecs+customerio&type=commits to find the pods that successfully deployed. Dont trust cocoapods.org or the logs from this script if a deployment was successful or not."
 echo "2. Feel free to re-run a GitHub Action if you see errors. This script can be run many times and not cause issues with pods that have already been deployed."
 
-pod repo update 
-pod trunk push "$PODSPEC" --allow-warnings --synchronous
+# the '|| true' code makes it so the command never fails, even if an error is returned. 
+# CocoaPods deployments are flaky. Because of that, when you deploy to cocoapods it's best that you manually confirm 
+# that the pods all got deployed successfully. If not, just re-run the job on github actions to try pushing the pods again. 
+# If you try to re-run the github action without '|| true', the script would fail early and not allow you to retry pushing all pods. 
+pod trunk push "$PODSPEC" --allow-warnings --synchronous || true 
