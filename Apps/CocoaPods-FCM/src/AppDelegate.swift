ab1c3f1a163cb6415ae57ff168ea8dfca8cb2a66
Levi Bostian <levi@customer.io>
2023-05-09 11:04:29 -0500

    get device token from FCM

--- Apps/CocoaPods-FCM/src/AppDelegate.swift
+++ Apps/CocoaPods-FCM/src/AppDelegate.swift
@@ -8,13 +8,18 @@ import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Follow setup guide for setting up FCM push: https://firebase.google.com/docs/cloud-messaging/ios/client
+        // The FCM SDK provides a device token to the app that you then send to the Customer.io SDK.
+
+        // First, initialize your SDKs.
+        // Initialize the Firebase SDK.
         FirebaseApp.configure()
 
         let appSetSettings = CioSettingsManager().appSetSettings
-        let siteId = appSetSettings?.siteId ?? ""
-        let apiKey = appSetSettings?.apiKey ?? ""
+        let siteId = appSetSettings?.siteId ?? "foo"
+        let apiKey = appSetSettings?.apiKey ?? "bar"
 
-        // we are only using this sample app for testing it can compile so providing a siteid and apikey is not useful at the moment.
+        // Initialize the Customer.io SDK
         CustomerIO.initialize(siteId: siteId, apiKey: apiKey, region: .US) { config in
             // Modify properties in the config object to configure the Customer.io SDK.
 
@@ -24,16 +29,28 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             appSetSettings?.configureCioSdk(config: &config)
         }
 
-        UIApplication.shared.registerForRemoteNotifications()
+        // Now that the Firebase and Customer.io SDK's are initialized, follow the rest of the required steps for the FCM push setup.
+        UNUserNotificationCenter.current().delegate = self
 
         Messaging.messaging().delegate = self
 
         return true
     }
+
+    // Because this is a SwiftUI app, we need to add this function to inform FCM about an APN token being registered.
+    // Without this function, the FCM delegate will not be called with a FCM token registered.
+    // Docs: https://firebase.google.com/docs/cloud-messaging/ios/client#token-swizzle-disabled
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {}
+
 extension AppDelegate: MessagingDelegate {
+    // FCM SDK calls this function when a FCM device token is available.
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        // Pass the FCM token to the Customer.io SDK:
         MessagingPush.shared.registerDeviceToken(fcmToken: fcmToken)
     }
 }
