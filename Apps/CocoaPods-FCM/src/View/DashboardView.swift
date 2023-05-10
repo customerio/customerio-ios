8c07998f5f8b7fc7ed1d97ffaa54193855794288
Levi Bostian <levi@customer.io>
2023-05-10 14:09:55 -0500

    add a note [skip ci]

--- Apps/CocoaPods-FCM/src/View/DashboardView.swift
+++ Apps/CocoaPods-FCM/src/View/DashboardView.swift
@@ -141,6 +141,8 @@ struct DashboardView: View {
             UNUserNotificationCenter.current().getNotificationSettings { settings in
                 showAskForPushPermissionButton = settings.authorizationStatus == .notDetermined
             }
+            // App opens via Universal Link.
+            // Any URL that begins with `https://ciosample.page.link` will open this app and display the URL to you in a pop-up.
         }.onOpenURL { deepLink in
             openedDeepLinkUrl = deepLink
             showOpenedDeepLink = true
