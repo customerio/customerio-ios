797822014bc39bfda2ca6f889c16eee2b2d3841a
Levi Bostian <levi@customer.io>
2023-05-11 09:17:11 -0500

    refactor: fcm sample enable in-app

--- Apps/CocoaPods-FCM/src/App.swift
+++ Apps/CocoaPods-FCM/src/App.swift
@@ -14,27 +14,26 @@ struct MainApp: App {
                 if userManager.isUserLoggedIn {
                     DashboardView()
                         .environmentObject(userManager)
-                        .accentColor(Color("AccentColor")) // sets Color.accentColor for all children
                 } else {
                     LoginView()
                         .environmentObject(userManager)
-                        .accentColor(Color("AccentColor")) // sets Color.accentColor for all children
                 }
-            }.onOpenURL { deepLink in // This function is how to implement deep links in a SwiftUIU app.
-                // This app opens deep links using Universal Links and app scheme deep links.
-                //
-                // Universal Links: Any URL that begins with `https://ciosample.page.link`...
-                // App scheme: Any URL that begins with `cocoapods-fcm://`...
-                //
-                // ...will open the app and display the deep link in a pop-up.
-                openedDeepLinkUrl = deepLink
-            }.alert(isPresented: Binding<URL>.notNil(openedDeepLinkUrl)) {
-                Alert(
-                    title: Text("Deep link opened!"),
-                    message: Text(openedDeepLinkUrl!.absoluteString),
-                    dismissButton: .default(Text("OK"))
-                )
-            }
+            }.accentColor(Color("AccentColor")) // sets Color.accentColor for all children
+                .onOpenURL { deepLink in // This function is how to implement deep links in a SwiftUIU app.
+                    // This app opens deep links using Universal Links and app scheme deep links.
+                    //
+                    // Universal Links: Any URL that begins with `https://ciosample.page.link`...
+                    // App scheme: Any URL that begins with `cocoapods-fcm://`...
+                    //
+                    // ...will open the app and display the deep link in a pop-up.
+                    openedDeepLinkUrl = deepLink
+                }.alert(isPresented: Binding<URL>.notNil(openedDeepLinkUrl)) {
+                    Alert(
+                        title: Text("Deep link opened!"),
+                        message: Text(openedDeepLinkUrl!.absoluteString),
+                        dismissButton: .default(Text("OK"))
+                    )
+                }
         }
     }
 }
