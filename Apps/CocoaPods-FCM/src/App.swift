ef534c2a70aefefbeba082a1b579b6f99e5f1231
Levi Bostian <levi@customer.io>
2023-05-15 14:04:24 -0500

    refactor: fcm sample deep links (#310)

--- Apps/CocoaPods-FCM/src/App.swift
+++ Apps/CocoaPods-FCM/src/App.swift
@@ -6,16 +6,34 @@ struct MainApp: App {
 
     @StateObject var userManager: UserManager = .init()
 
+    @State private var openedDeepLinkUrl: URL?
+
     var body: some Scene {
         WindowGroup {
-            if userManager.isUserLoggedIn {
-                DashboardView()
-                    .environmentObject(userManager)
-                    .accentColor(Color("AccentColor")) // sets Color.accentColor for all children
-            } else {
-                LoginView()
-                    .environmentObject(userManager)
-                    .accentColor(Color("AccentColor")) // sets Color.accentColor for all children
+            HStack {
+                if userManager.isUserLoggedIn {
+                    DashboardView()
+                        .environmentObject(userManager)
+                        .accentColor(Color("AccentColor")) // sets Color.accentColor for all children
+                } else {
+                    LoginView()
+                        .environmentObject(userManager)
+                        .accentColor(Color("AccentColor")) // sets Color.accentColor for all children
+                }
+            }.onOpenURL { deepLink in // This function is how to implement deep links in a SwiftUIU app.
+                // This app opens deep links using Universal Links and app scheme deep links.
+                //
+                // Universal Links: Any URL that begins with `https://ciosample.page.link`...
+                // App scheme: Any URL that begins with `cocoapods-fcm://`...
+                //
+                // ...will open the app and display the deep link in a pop-up.
+                openedDeepLinkUrl = deepLink
+            }.alert(isPresented: Binding<URL>.notNil(openedDeepLinkUrl)) {
+                Alert(
+                    title: Text("Deep link opened!"),
+                    message: Text(openedDeepLinkUrl!.absoluteString),
+                    dismissButton: .default(Text("OK"))
+                )
             }
         }
     }
