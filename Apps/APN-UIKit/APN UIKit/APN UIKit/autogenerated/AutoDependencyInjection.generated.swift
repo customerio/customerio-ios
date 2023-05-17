b23a06259273aed4c516c3c106559bf3fd3b5a0c
Shahroz Khan <shrozali@gmail.com>
2023-05-17 21:50:48 +0500

    Revert "autogen"
    This reverts commit 2122bb6e7188771e070467ec88af02e0fb89fa14.

--- Apps/APN-UIKit/APN UIKit/APN UIKit/autogenerated/AutoDependencyInjection.generated.swift
+++ Apps/APN-UIKit/APN UIKit/APN UIKit/autogenerated/AutoDependencyInjection.generated.swift
@@ -2,104 +2,108 @@
 // DO NOT EDIT
 // swiftlint:disable all
 
-import Foundation
-import CioTracking
 import CioMessagingPushAPN
+import CioTracking
+import Foundation
 
 /**
-######################################################
-Documentation
-######################################################
+ ######################################################
+ Documentation
+ ######################################################
 
-This automatically generated file you are viewing is a dependency injection graph for your app's source code.
-You may be wondering a couple of questions. 
+ This automatically generated file you are viewing is a dependency injection graph for your app's source code.
+ You may be wondering a couple of questions.
 
-1. How did this file get generated? Answer --> https://github.com/levibostian/Sourcery-DI#how
-2. Why use this dependency injection graph instead of X other solution/tool? Answer --> https://github.com/levibostian/Sourcery-DI#why-use-this-project
-3. How do I add dependencies to this graph file? Follow one of the instructions below:
-* Add a non singleton class: https://github.com/levibostian/Sourcery-DI#add-a-non-singleton-class
-* Add a generic class: https://github.com/levibostian/Sourcery-DI#add-a-generic-class
-* Add a singleton class: https://github.com/levibostian/Sourcery-DI#add-a-singleton-class
-* Add a class from a 3rd party library/SDK: https://github.com/levibostian/Sourcery-DI#add-a-class-from-a-3rd-party
-* Add a `typealias` https://github.com/levibostian/Sourcery-DI#add-a-typealias
+ 1. How did this file get generated? Answer --> https://github.com/levibostian/Sourcery-DI#how
+ 2. Why use this dependency injection graph instead of X other solution/tool? Answer --> https://github.com/levibostian/Sourcery-DI#why-use-this-project
+ 3. How do I add dependencies to this graph file? Follow one of the instructions below:
+ * Add a non singleton class: https://github.com/levibostian/Sourcery-DI#add-a-non-singleton-class
+ * Add a generic class: https://github.com/levibostian/Sourcery-DI#add-a-generic-class
+ * Add a singleton class: https://github.com/levibostian/Sourcery-DI#add-a-singleton-class
+ * Add a class from a 3rd party library/SDK: https://github.com/levibostian/Sourcery-DI#add-a-class-from-a-3rd-party
+ * Add a `typealias` https://github.com/levibostian/Sourcery-DI#add-a-typealias
 
-4. How do I get dependencies from the graph in my code? 
-```
-// If you have a class like this:
-class OffRoadWheels {}
+ 4. How do I get dependencies from the graph in my code?
+ ```
+ // If you have a class like this:
+ class OffRoadWheels {}
 
-class ViewController: UIViewController {
-    // Call the property getter to get your dependency from the graph: 
-    let wheels = DIGraph.getInstance(siteId: "").offRoadWheels
-    // note the name of the property is name of the class with the first letter lowercase. 
-}
-```
+ class ViewController: UIViewController {
+     // Call the property getter to get your dependency from the graph:
+     let wheels = DIGraph.getInstance(siteId: "").offRoadWheels
+     // note the name of the property is name of the class with the first letter lowercase.
+ }
+ ```
 
-5. How do I use this graph in my test suite? 
-```
-let mockOffRoadWheels = // make a mock of OffRoadWheels class 
-DIGraph().override(mockOffRoadWheels, OffRoadWheels.self) 
-```
+ 5. How do I use this graph in my test suite?
+ ```
+ let mockOffRoadWheels = // make a mock of OffRoadWheels class
+ DIGraph().override(mockOffRoadWheels, OffRoadWheels.self)
+ ```
 
-Then, when your test function finishes, reset the graph:
-```
-DIGraph().reset()
-```
+ Then, when your test function finishes, reset the graph:
+ ```
+ DIGraph().reset()
+ ```
 
-*/
-
-
+ */
 
 extension DIGraph {
-    // call in automated test suite to confirm that all dependnecies able to resolve and not cause runtime exceptions. 
-    // internal scope so each module can provide their own version of the function with the same name. 
-    @available(iOSApplicationExtension, unavailable) // some properties could be unavailable to app extensions so this function must also. 
-    internal func testDependenciesAbleToResolve() -> Int {
+    // call in automated test suite to confirm that all dependnecies able to resolve and not cause runtime exceptions.
+    // internal scope so each module can provide their own version of the function with the same name.
+    @available(iOSApplicationExtension, unavailable) // some properties could be unavailable to app extensions so this function must also.
+    func testDependenciesAbleToResolve() -> Int {
         var countDependenciesResolved = 0
 
-        _ = self.deepLinksHandlerUtil
+        _ = deepLinksHandlerUtil
         countDependenciesResolved += 1
 
-        _ = self.notificationUtil
+        _ = notificationUtil
         countDependenciesResolved += 1
 
-        _ = self.storage
+        _ = storage
         countDependenciesResolved += 1
 
-        _ = self.userDefaults
+        _ = userDefaults
         countDependenciesResolved += 1
 
-        return countDependenciesResolved    
+        return countDependenciesResolved
     }
 
     // DeepLinksHandlerUtil
-    internal var deepLinksHandlerUtil: DeepLinksHandlerUtil {  
-        return getOverriddenInstance() ??
-            self.newDeepLinksHandlerUtil
+    var deepLinksHandlerUtil: DeepLinksHandlerUtil {
+        getOverriddenInstance() ??
+            newDeepLinksHandlerUtil
     }
-    private var newDeepLinksHandlerUtil: DeepLinksHandlerUtil {    
-        return AppDeepLinksHandlerUtil()
+
+    private var newDeepLinksHandlerUtil: DeepLinksHandlerUtil {
+        AppDeepLinksHandlerUtil()
     }
+
     // NotificationUtil
-    internal var notificationUtil: NotificationUtil {  
-        return getOverriddenInstance() ??
-            self.newNotificationUtil
+    var notificationUtil: NotificationUtil {
+        getOverriddenInstance() ??
+            newNotificationUtil
     }
-    private var newNotificationUtil: NotificationUtil {    
-        return NotificationUtil()
+
+    private var newNotificationUtil: NotificationUtil {
+        NotificationUtil()
     }
+
     // Storage
-    internal var storage: Storage {  
-        return getOverriddenInstance() ??
-            self.newStorage
+    var storage: Storage {
+        getOverriddenInstance() ??
+            newStorage
     }
-    private var newStorage: Storage {    
-        return Storage(userDefaults: self.userDefaults)
+
+    private var newStorage: Storage {
+        Storage(userDefaults: userDefaults)
     }
+
     // UserDefaults (custom. property getter provided via extension)
-    internal var userDefaults: UserDefaults {  
-        return getOverriddenInstance() ??
-            self.customUserDefaults
+    var userDefaults: UserDefaults {
+        getOverriddenInstance() ??
+            customUserDefaults
     }
 }
 
