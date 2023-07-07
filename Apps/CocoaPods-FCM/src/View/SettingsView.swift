35bbac0c47bef5315db77ba803a9cbb317b9631f
Levi Bostian <levi@customer.io>
2023-07-07 10:54:09 -0500

    validate tracking URL

--- Apps/CocoaPods-FCM/src/View/SettingsView.swift
+++ Apps/CocoaPods-FCM/src/View/SettingsView.swift
@@ -11,6 +11,8 @@ struct SettingsView: View {
 
     @StateObject private var viewModel = ViewModel()
 
+    @State private var alertMessage: String?
+
     @EnvironmentObject var userManager: UserManager
 
     var body: some View {
@@ -31,7 +33,9 @@ struct SettingsView: View {
                 }
 
                 Group {
-                    LabeledTextField(title: "Tracking URL:", value: $viewModel.settings.trackUrl).setAppiumId("Track URL Input")
+                    LabeledTextField(title: "Tracking URL:", value: $viewModel.settings.trackUrl)
+                        .autocapitalization(.none)
+                        .setAppiumId("Track URL Input")
                     LabeledTextField(title: "Site id:", value: $viewModel.settings.siteId).setAppiumId("Site ID Input")
                     LabeledTextField(title: "API key:", value: $viewModel.settings.apiKey).setAppiumId("API Key Input")
                     LabeledTextField(title: "BQ seconds delay:", value: $viewModel.settings.bqSecondsDelay.toStringBinding())
@@ -42,6 +46,10 @@ struct SettingsView: View {
                 }
 
                 ColorButton("Save") {
+                    guard verifyTrackUrl() else {
+                        return
+                    }
+
                     // save settings to device storage for app to re-use when app is restarted
                     let didChangeSiteId = viewModel.saveSettings()
 
@@ -70,7 +78,48 @@ struct SettingsView: View {
                     viewModel.settings.apiKey = apiKey
                 }
             }
+            .alert(isPresented: .notNil(alertMessage)) {
+                Alert(
+                    title: Text("Error"),
+                    message: Text(alertMessage!),
+                    dismissButton: .default(Text("OK")) {
+                        alertMessage = nil
+                    }
+                )
+            }
         }
+    }
+
+    private func verifyTrackUrl() -> Bool {
+        let enteredUrl = viewModel.settings.trackUrl
+
+        guard !enteredUrl.isEmpty else {
+            alertMessage = "Tracking URL is empty. Therefore, I cannot save the settings."
+            return false
+        }
+
+        guard let url = URL(string: enteredUrl) else {
+            alertMessage = "Tracking URL, \(enteredUrl), is not a valid URL. Therefore, I cannot save the settings."
+            return false
+        }
+
+        guard url.scheme != nil, url.scheme == "https" || url.scheme == "http" else {
+            alertMessage = "Tracking URL, \(enteredUrl), does not start with https or http. Therefore, I cannot save the settings."
+            return false
+        }
+
+        guard url.host != nil, !url.host!.isEmpty else {
+            alertMessage = "Tracking URL, \(enteredUrl), does not contain a domain name. Therefore, I cannot save the settings."
+            return false
+        }
+
+        // Auto-fix this instead of making the user fix it.
+        let urlEndsWithTrailingSlash = url.absoluteString.hasSuffix("/")
+        if !urlEndsWithTrailingSlash {
+            viewModel.settings.trackUrl += "/"
+        }
+
+        return true
     }
 
     class ViewModel: ObservableObject {
