976c7e09927a528d9c5003a90a3712f5dd0bc163
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-07-13 23:44:05 +0530

    identifier not empty check
    Co-authored-by: Levi Bostian <levi@customer.io>

--- Sources/Tracking/CustomerIOImplementation.swift
+++ Sources/Tracking/CustomerIOImplementation.swift
@@ -225,8 +225,12 @@ internal class CustomerIOImplementation: CustomerIOInstance {
         // we can reference the token and register it to a new profile.
         globalDataStore.pushDeviceToken = deviceToken
 
-        guard let identifier = profileStore.identifier, !identifier.isEmpty else {
+        guard let identifier = profileStore.identifier else {
             logger.info("no profile identified, so not registering device token to a profile")
+            return
+        }
+        guard !identifier.isEmpty else {
+            logger.error("profiles cannot be created without an identifier. Empty string was provided as the identifier, therefore, request to identify profile in the SDK is ignored. Try identifying a profile again with a non-empty identifier value.")
             return
         }
         // OS name might not be available if running on non-apple product. We currently only support iOS for the SDK
