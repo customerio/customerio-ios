ec7d9747fc1b77fb74719fe45e28fc4079401419
Levi Bostian <levi@customer.io>
2023-07-19 14:06:40 -0500

    refactor: fix script for SDK wrappers to install non-prod versions of SDK

--- scripts/cocoapods_override_sdk.rb
+++ scripts/cocoapods_override_sdk.rb
@@ -9,54 +9,68 @@ require 'open-uri'
 IO.copy_stream(URI.open('https://raw.githubusercontent.com/customerio/customerio-ios/main/scripts/cocoapods_override_sdk.rb'), "/tmp/override_cio_sdk.rb")
 load "/tmp/override_cio_sdk.rb"
 
+# Required by native iOS FCM SDK. 
+# Uncomment the line below if you are installing any of the FCM CIO pods. 
+# use_frameworks! :linkage => :static
+
 target 'SampleApp' do
   # Uncomment only 1 of the lines below to install a version of the iOS SDK 
   pod 'CustomerIO/MessagingPushAPN', '~> 2.1' # install production build 
-  # install_non_production_ios_sdk_local_path('~/code/customerio-ios/')
-  # install_non_production_ios_sdk_git_branch('name-of-ios-sdk-branch')
+  # install_non_production_ios_sdk_local_path('~/code/customerio-ios/', is_app_extension: false, push_service: "apn")
+  # install_non_production_ios_sdk_git_branch('name-of-ios-sdk-branch', is_app_extension: false, push_service: "apn")
 end
 
 target 'Notification Service' do
   # Uncomment only 1 of the lines below to install a version of the iOS SDK 
   pod 'CustomerIO/MessagingPushAPN', '~> 2.1' # install production build 
-  # install_non_production_ios_sdk_local_path('~/code/customerio-ios/', is_app_extension: true)
-  # install_non_production_ios_sdk_git_branch('name-of-ios-sdk-branch', is_app_extension: true)
+  # install_non_production_ios_sdk_local_path('~/code/customerio-ios/', is_app_extension: true, push_service: "apn")
+  # install_non_production_ios_sdk_git_branch('name-of-ios-sdk-branch', is_app_extension: true, push_service: "apn")
 end
 
 =end 
 
-def install_non_production_ios_sdk_git_branch(branch_name, is_app_extension: false)
+def install_non_production_ios_sdk_git_branch(branch_name, is_app_extension, push_service)
   puts ""
   puts "⚠️ Installing CIO iOS SDK from git branch #{branch_name}"
   puts ""
 
-  get_all_cio_pods(is_app_extension: is_app_extension).each { |podname| 
+  get_all_cio_pods(is_app_extension, push_service).each { |podname| 
     pod podname, :git => "https://github.com/customerio/customerio-ios.git", :branch => branch_name
   }  
 end 
 
-def install_non_production_ios_sdk_local_path(local_path, is_app_extension: false)
+def install_non_production_ios_sdk_local_path(local_path, is_app_extension, push_service)
   local_path = File.expand_path(local_path, Dir.pwd)
 
   puts ""
   puts "⚠️ Installing local version of the iOS SDK. Path of iOS SDK: #{local_path}"
   puts ""
 
-  get_all_cio_pods(is_app_extension: is_app_extension).each { |podname| 
+  get_all_cio_pods(is_app_extension, push_service).each { |podname| 
     pod podname, :path => local_path
   }
 end 
 
 # In iOS App Extensions (example: Notification Service Extensions for rich push), there are some pods we do not want to install. If we do, it might cause our app to no longer compile. 
-def get_all_cio_pods(is_app_extension)
+def get_all_cio_pods(is_app_extension, push_service)
   # All of these pods are App Extension compatible. We can install them all in an App Extension or a host app. 
   pods_for_all_targets = [
     'CustomerIOCommon',
     'CustomerIOTracking',    
-    'CustomerIOMessagingPush',
-    'CustomerIOMessagingPushAPN',
-    'CustomerIOMessagingPushFCM'
+    'CustomerIOMessagingPush'
   ]
+
+  if push_service == "apn" 
+    pods_for_all_targets.push('CustomerIOMessagingPushAPN')
+  end 
+
+  # native iOS FCM SDK requires that you add this to your Podfile...
+  # use_frameworks! :linkage => :static 
+  # ...For apps that only use APN, we do not want to modify the Podfile to not cause your pod install to break. 
+  # Therefore, only install the FCM pod if you need it for your app. 
+  if push_service == "fcm"
+    pods_for_all_targets.push('CustomerIOMessagingPushFCM') 
+  end 
 
   if !is_app_extension     
     # Gist SDK is not setup to work with App Extensions. Therefore, only install it if we are not in an App extension. 
