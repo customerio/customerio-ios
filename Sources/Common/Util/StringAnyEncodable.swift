30220546b9a1cd1792a1d2343098b3ef0d88bbab
Rehan <mrehan.official@gmail.com>
2023-07-24 15:47:05 +0500

    fixed formatting

--- Sources/Common/Util/StringAnyEncodable.swift
+++ Sources/Common/Util/StringAnyEncodable.swift
@@ -9,17 +9,17 @@ public struct StringAnyEncodable: Encodable {
             switch value {
             case let enc as Encodable:
                 return AnyEncodable(enc)
-            
+
             case let dict as [String: Any]:
                 return AnyEncodable(StringAnyEncodable(dict))
-            
+
             case let list as [Any]:
                 // If the value is an array, recursively encode each element
                 return AnyEncodable(list.compactMap { encode(value: $0) })
 
             default:
                 // XXX: logger error
-                return nil;
+                return nil
             }
         }
 
