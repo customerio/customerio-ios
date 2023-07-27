ea0a93244b3edd592631030ddcdc57340390b48c
Levi Bostian <levi@levi.earth>
2023-07-27 15:02:11 -0500

    Discard changes to Sources/Common/Extensions/UserDefaultExtensions.swift

--- Sources/Common/Extensions/UserDefaultExtensions.swift
+++ Sources/Common/Extensions/UserDefaultExtensions.swift
@@ -4,12 +4,4 @@ internal extension UserDefaults {
     func deleteAll() {
         dictionaryRepresentation().keys.forEach { removeObject(forKey: $0) }
     }
-
-    func copyValuesFrom(source: UserDefaults) {
-        for key in source.dictionaryRepresentation().keys {
-            if let value = source.value(forKey: key) {
-                setValue(value, forKey: key)
-            }
-        }
-    }
 }
