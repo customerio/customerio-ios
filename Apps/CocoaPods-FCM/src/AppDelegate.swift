15ee4f2756109a9ac271a4e02ad7842a0acb76ef
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-08-28 20:38:03 +0530

    testing fcm

--- Apps/CocoaPods-FCM/src/AppDelegate.swift
+++ Apps/CocoaPods-FCM/src/AppDelegate.swift
@@ -35,6 +35,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
         Messaging.messaging().delegate = self
 
+        application.registerForRemoteNotifications()
         return true
     }
 
