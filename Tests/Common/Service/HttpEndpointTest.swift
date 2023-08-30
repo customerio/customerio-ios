f081cf656a89e0b730df04152cc2ba668daf0ae9
Shahroz Khan <shrozali@gmail.com>
2023-08-31 01:17:25 +0500

    added tests

--- Tests/Common/Service/HttpEndpointTest.swift
+++ Tests/Common/Service/HttpEndpointTest.swift
@@ -44,4 +44,29 @@ class HttpEndpointTest: UnitTest {
 
         XCTAssertEqual(actual, expected)
     }
+
+    func test_getUrlString_givenRandomPathNeedsEncoding_expectEncodedPath() {
+        let identifierWithSpecialChar = String.randomStringWithSpecialCharacters()
+        let endpoint = CIOApiEndpoint.identifyCustomer(identifier: identifierWithSpecialChar)
+
+        let expectedIdentifier = identifierWithSpecialChar.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? identifierWithSpecialChar
+        let expected = "https://customer.io/api/v1/customers/\(expectedIdentifier)"
+
+        setHttpBaseUrls(trackingApi: "https://customer.io")
+
+        let actual = endpoint.getUrlString(baseUrls: httpBaseUrls)
+
+        XCTAssertEqual(actual, expected)
+    }
+
+    func test_getUrl_givenUnencodedPathWithSpecialCharacter_expectValidUrl() {
+        let identifierWithSpecialChar = "social-login|1234567890abcde"
+        let endpoint = CIOApiEndpoint.identifyCustomer(identifier: identifierWithSpecialChar)
+
+        setHttpBaseUrls(trackingApi: "https://customer.io")
+
+        let actualUrl = endpoint.getUrl(baseUrls: httpBaseUrls)
+
+        XCTAssertNotNil(actualUrl, "Expected valid URL but got nil. Ensure path is encoded correctly.")
+    }
 }
