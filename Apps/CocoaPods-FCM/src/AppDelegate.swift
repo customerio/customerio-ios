fcdad741a58c44cbaa2c991bf28379d922bb8af8
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-09-01 20:05:37 +0530

    removed

--- Apps/CocoaPods-FCM/src/AppDelegate.swift
+++ Apps/CocoaPods-FCM/src/AppDelegate.swift
@@ -35,7 +35,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
         Messaging.messaging().delegate = self
 
-//        application.registerForRemoteNotifications()
         return true
     }
 
