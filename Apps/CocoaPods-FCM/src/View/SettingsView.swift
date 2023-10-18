6c1ee1e572d46bc41ec2585d561275a809918ba3
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-10-18 19:42:43 +0400

    removed commented code

--- Apps/CocoaPods-FCM/src/View/SettingsView.swift
+++ Apps/CocoaPods-FCM/src/View/SettingsView.swift
@@ -156,7 +156,7 @@ struct SettingsView: View {
         init() {
             self.settingsManager = CioSettingsManager()
             self.keyValueStorage = KeyValueStore()
-            self.pushToken = CustomerIO.shared.registeredDeviceToken() ?? "(none)" // keyValueStorage.pushToken ?? "(none)"
+            self.pushToken = CustomerIO.shared.registeredDeviceToken() ?? "(none)"
             self.settings = settingsManager.appSetSettings ?? CioSettings.getFromCioSdk()
         }
 
