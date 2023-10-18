86a5a0b33aa7dee3be683218a5ec441876b9a3b5
ami-aman <91549653+ami-aman@users.noreply.github.com>
2023-10-18 19:41:51 +0400

    clean FCM push config code

--- Sources/MessagingPushFCM/MessagingPushFCM+PushConfigs.swift
+++ Sources/MessagingPushFCM/MessagingPushFCM+PushConfigs.swift
@@ -8,12 +8,6 @@ import UIKit
 extension MessagingPushFCM {
     @available(iOSApplicationExtension, unavailable)
     func setupAutoFetchDeviceToken() {
-        // Set delegate
-        //        Messaging.messaging().delegate = Self
-
-        // Swizzle method `didRegisterForRemoteNotificationsWithDeviceToken`
-        //        swizzleDidRegisterForRemoteNotifications()
-        // Trying to fetch token again using firebase's methods
         swizzleDidRegisterForRemoteNotifications()
     }
 
@@ -37,6 +31,7 @@ extension MessagingPushFCM {
     }
 
     // Swizzled method for APN device token.
+    // Fetch the FCM token using the Firebase delegate method when the APN token is set.
     @objc
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
@@ -45,46 +40,7 @@ extension MessagingPushFCM {
                 print("SOME ERROR")
                 return
             }
-            // register token
+            Self.shared.registerDeviceToken(fcmToken: token)
         })
-        // Self.shared.registerDeviceToken(apnDeviceToken: deviceToken)
     }
 }
-
-/*
-     @available(iOSApplicationExtension, unavailable)
-     private func swizzleDidRegisterForRemoteNotifications() {
-         let appDelegate = MessagingDelegate.self
-         let appDelegateClass: AnyClass? = object_getClass(appDelegate)
-         let originalSelector = #selector(MessagingDelegate.messaging(_:didReceiveRegistrationToken:))
-         let swizzledSelector = #selector(MessagingPushFCM.messaging(_:didReceiveRegistrationToken:))
-         swizzle(forOriginalClass: appDelegateClass, forSwizzledClass: MessagingPushFCM.self, original: originalSelector, new: swizzledSelector)
-     }
-
-     private func swizzle(forOriginalClass: AnyClass?, forSwizzledClass: AnyClass?, original: Selector, new: Selector) {
-         guard let swizzledMethod = class_getInstanceMethod(forSwizzledClass, new) else { return }
-         guard let originalMethod = class_getInstanceMethod(forOriginalClass, original) else {
-             // Add method if it doesn't exist
-             class_addMethod(forOriginalClass, original, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
-             return
-         }
-         method_exchangeImplementations(originalMethod, swizzledMethod)
-     }
-
-     // Swizzled method for FCM device token.
-     @objc
-     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-         print(fcmToken)
- //        Self.shared.registerDeviceToken(apnDeviceToken: deviceToken)
-     }
- }
-
- //
- // extension MessagingPushFCM: MessagingDelegate {
- //    // FCM SDK calls this function when a FCM device token is available.
- //    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
- //        // Pass the FCM token to the Customer.io SDK:
- //        MessagingPush.shared.registerDeviceToken(fcmToken: fcmToken)
- //    }
- // }
- */
