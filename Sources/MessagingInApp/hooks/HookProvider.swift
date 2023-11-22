d1a6714865cd2ca725d5990c22821c9f6a2b332e
Rehan <mrehan.official@gmail.com>
2023-11-22 13:34:35 +0500

    hooks update

--- Sources/MessagingInApp/hooks/HookProvider.swift
+++ Sources/MessagingInApp/hooks/HookProvider.swift
@@ -1,13 +1,11 @@
 import CioInternalCommon
-import CioTracking
 import Foundation
 
 // sourcery: InjectRegister = "ModuleHookProvider"
 class MessagingInAppModuleHookProvider: ModuleHookProvider {
-    private let sdkInitializedUtil = SdkInitializedUtilImpl()
-
     private var diGraph: DIGraph? {
-        sdkInitializedUtil.postInitializedData?.diGraph
+        // TODO Fix DIGraph
+        nil
     }
 
     var profileIdentifyHook: ProfileIdentifyHook? {
