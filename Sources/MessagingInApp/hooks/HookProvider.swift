4b657c3ed40745e3a46b6184d018e9900a08f015
Rehan <mrehan.official@gmail.com>
2023-11-24 20:41:29 +0500

    comment update

--- Sources/MessagingInApp/hooks/HookProvider.swift
+++ Sources/MessagingInApp/hooks/HookProvider.swift
@@ -11,14 +11,14 @@ class MessagingInAppModuleHookProvider: ModuleHookProvider {
     var profileIdentifyHook: ProfileIdentifyHook? {
         guard let diGraph = diGraph else { return nil }
 
-        // FIXME: [CDP] Find workaround or reuse existing instance to utilize customer provided siteid
+        // FIXME: [CDP] Find workaround by attaching hook to Journeys or reusing existing instance to utilize customer provided siteid
         return MessagingInAppImplementation(diGraph: diGraph, moduleConfig: MessagingInAppConfigOptions.Factory.create())
     }
 
     var screenTrackingHook: ScreenTrackingHook? {
         guard let diGraph = diGraph else { return nil }
 
-        // FIXME: [CDP] Find workaround or reuse existing instance to utilize customer provided siteid
+        // FIXME: [CDP] Find workaround by attaching hook to Journeys or reusing existing instance to utilize customer provided siteid
         return MessagingInAppImplementation(diGraph: diGraph, moduleConfig: MessagingInAppConfigOptions.Factory.create())
     }
 }
