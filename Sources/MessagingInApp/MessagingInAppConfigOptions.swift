ba7748b3e478fbfd9955e8f5850d2c94a15b320d
Rehan <mrehan.official@gmail.com>
2023-11-24 20:37:20 +0500

    inapp config

--- Sources/MessagingInApp/MessagingInAppConfigOptions.swift
+++ Sources/MessagingInApp/MessagingInAppConfigOptions.swift
@@ -16,9 +16,25 @@ public struct MessagingInAppConfigOptions {
     public enum Factory {
         // TODO: [CDP] Rethink on how can we eliminate default value and force required properties
         public static func create(siteId: String = "", region: Region = Region.US) -> MessagingInAppConfigOptions {
-            MessagingInAppConfigOptions(
+            return MessagingInAppConfigOptions(
                 siteId: siteId,
                 region: region
+            )
+        }
+
+        public static func create(with dictionary: [String: Any]) -> MessagingInAppConfigOptions {
+            // Each SDK config option should be able to be set from `dictionary`.
+            // If one isn't provided, use current value instead.
+            
+            // Construct object with all required parameters. Each config option should be updated from `dictionary` only if available.
+            let siteId = dictionary[Keys.siteId.rawValue] as? String
+            let region = (dictionary[Keys.region.rawValue] as? String).map(Region.getRegion)
+
+            // Use default config options as fallback
+            let presetConfig = create()
+            return MessagingInAppConfigOptions(
+                siteId: siteId ?? presetConfig.siteId,
+                region: region ?? presetConfig.region
             )
         }
     }
@@ -34,26 +50,4 @@ public struct MessagingInAppConfigOptions {
     /// Property to store workspace Region set during module initialization
     /// Immutable for lazy updates only, can be updated using initialize method
     public private(set) var region: Region
-
-    public mutating func initialize(siteId: String, region: Region? = nil) {
-        self.siteId = siteId
-        if let region = region {
-            self.region = region
-        }
-    }
-    
-    mutating func apply(with dictionary: [String: Any]) {
-        // Each SDK config option should be able to be set from `dictionary`.
-        // If one isn't provided, use current value instead.
-        
-        // If a parameter takes more logic to calculate, perform the logic up here.
-        if let regionStringValue = dictionary[Keys.region.rawValue] as? String {
-            self.region = Region.getRegion(from: regionStringValue)
-        }
-
-        // Construct object with all required parameters. Each config option should be updated from `dictionary` only if available.
-        if let siteId = dictionary[Keys.siteId.rawValue] as? String {
-            self.siteId = siteId
-        }
-    }
 }
