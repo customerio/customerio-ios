28ff3314553429911bb3d97700713ffa696dda71
Shahroz Khan <shrozali@gmail.com>
2023-11-28 17:53:50 +0500

    pr suggestion

--- Sources/DataPipeline/CIODataPipelineConfig.swift
+++ Sources/DataPipeline/CIODataPipelineConfig.swift
@@ -3,6 +3,7 @@ import Foundation
 import Segment
 
 extension Configuration {
+    // TODO: Module Level configuration is going to be added here which will replace this config
     static var defaultConfiguration: Configuration {
         let siteId = CustomerIO.shared.config?.siteId ?? ""
         let apiKey = CustomerIO.shared.config?.apiKey ?? ""
