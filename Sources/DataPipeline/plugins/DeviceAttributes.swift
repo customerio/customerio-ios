a3d777b9c534ae1a8dd1d5986afc3930e0a56282
Rehan <mrehan.official@gmail.com>
2023-12-12 11:26:33 +0500

    DeviceAttributes plugin

---
+++ Sources/DataPipeline/plugins/DeviceAttributes.swift
@@ -0,0 +1,25 @@
+import CioInternalCommon
+import Segment
+
+class DeviceAttributes: Plugin {
+    public let type = PluginType.before
+    public weak var analytics: Analytics?
+
+    public var attributes: [String: Any]?
+
+    public required init() {}
+
+    public func execute<T: RawEvent>(event: T?) -> T? {
+        guard var workingEvent = event else { return event }
+
+        if var context = workingEvent.context?.dictionaryValue, let attributes = attributes {
+            context[keyPath: "device.attributes"] = attributes
+            do {
+                workingEvent.context = try JSON(context)
+            } catch {
+                analytics?.reportInternalError(error)
+            }
+        }
+        return workingEvent
+    }
+}
