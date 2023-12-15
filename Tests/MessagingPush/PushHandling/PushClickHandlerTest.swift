fb7b75d93563e9ab97692a31d4365131b161306b
Levi Bostian <levi@customer.io>
2023-12-15 11:12:33 -0600

    add tests

--- Tests/MessagingPush/PushHandling/PushClickHandlerTest.swift
+++ Tests/MessagingPush/PushHandling/PushClickHandlerTest.swift
@@ -6,7 +6,7 @@ import SharedTests
 import UserNotifications
 import XCTest
 
-class PushClickHandlerTest: UnitTest {
+class PushClickHandlerTest: IntegrationTest {
     private var pushClickHandler: PushClickHandler!
 
     private let deepLinkUtilMock = DeepLinkUtilMock()
@@ -93,21 +93,5 @@ class PushClickHandlerTest: UnitTest {
         pushHistoryMock.hasHandledPushClickReturnValue = false
         pushClickHandler.pushClicked(givenPush)
         XCTAssertEqual(customerIOMock.trackMetricCallsCount, 1)
-    }
-}
-
-extension PushClickHandlerTest {
-    func getPush(content: [AnyHashable: Any], deliveryId: String = .random, deviceToken: String = .random) -> CustomerIOParsedPushPayload {
-        var content = content
-
-        // swiftlint:disable:next force_cast
-        let notificationContent = UNNotificationContent().mutableCopy() as! UNMutableNotificationContent
-
-        content["CIO-Delivery-ID"] = deliveryId
-        content["CIO-Delivery-Token"] = deviceToken
-
-        notificationContent.userInfo = content
-
-        return CustomerIOParsedPushPayload.parse(notificationContent: notificationContent, jsonAdapter: jsonAdapter)!
     }
 }
