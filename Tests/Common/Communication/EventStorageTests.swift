04bc161bcaa7f6cc8c2f6c4371f8ecaadd385ca7
Shahroz Khan <shrozali@gmail.com>
2023-12-26 01:56:16 +0500

    added comments

--- Tests/Common/Communication/EventStorageTests.swift
+++ Tests/Common/Communication/EventStorageTests.swift
@@ -20,6 +20,8 @@ class EventStorageTest: UnitTest {
         super.tearDown()
     }
 
+    // MARK: - Helper Methods
+
     func createTemporaryDirectory() -> URL? {
         let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
         let uniqueDirectoryURL = temporaryDirectoryURL.appendingPathComponent(UUID().uuidString)
@@ -32,6 +34,8 @@ class EventStorageTest: UnitTest {
         }
     }
 
+    // MARK: - Event Storage Tests
+
     func testStoreEvent() async throws {
         let event = ProfileIdentifiedEvent(identifier: "testID")
         try await eventStorageManager.store(event: event)
@@ -43,6 +47,17 @@ class EventStorageTest: UnitTest {
         XCTAssertTrue(fileExists, "Event file should exist after storing")
     }
 
+    func testStoreCreatesDirectoryIfNeeded() async throws {
+        let event = ResetEvent() // Assume this is a new event type
+        try await eventStorageManager.store(event: event)
+
+        let eventTypeDirectory = await eventStorageManager.baseDirectory.appendingPathComponent(ResetEvent.key)
+        let directoryExists = FileManager.default.fileExists(atPath: eventTypeDirectory.path)
+        XCTAssertTrue(directoryExists, "Event storage should create the directory if it does not exist")
+    }
+
+    // MARK: - Event Retrieval Tests
+
     func testLoadStoredEvents() async throws {
         let event = ProfileIdentifiedEvent(identifier: "testID")
         try await eventStorageManager.store(event: event)
@@ -52,44 +67,15 @@ class EventStorageTest: UnitTest {
         XCTAssertTrue(containsEvent, "Loaded events should contain the stored event")
     }
 
-    func testRemoveEvent() async throws {
-        let event = ProfileIdentifiedEvent(identifier: "testID")
-        try await eventStorageManager.store(event: event)
-
-        let eventFileURL = await eventStorageManager.baseDirectory
-            .appendingPathComponent(ProfileIdentifiedEvent.key)
-            .appendingPathComponent("\(event.storageId).json")
-        await eventStorageManager.remove(ofType: ProfileIdentifiedEvent.key, withStorageId: event.storageId)
-
-        let fileExists = FileManager.default.fileExists(atPath: eventFileURL.path)
-        XCTAssertFalse(fileExists, "Event file should not exist after removal")
-    }
-
-    func testLoadEventsOfNonexistentType() async throws {
-        let loadedEvents = try await eventStorageManager.loadEvents(ofType: "NonexistentType")
-        XCTAssertTrue(loadedEvents.isEmpty, "Loading events for a nonexistent type should return an empty array")
-    }
-
     func testLoadEventsWhenDirectoryDoesNotExist() async throws {
         let nonExistentEventType = "NonExistentEvent"
         let loadedEvents = try await eventStorageManager.loadEvents(ofType: nonExistentEventType)
         XCTAssertTrue(loadedEvents.isEmpty, "Loading events for a non-existent directory should return an empty array")
     }
 
-    func testRemoveEventWhenFileDoesNotExist() async throws {
-        let eventType = ProfileIdentifiedEvent.key
-        let nonExistentStorageId = UUID().uuidString
-        await eventStorageManager.remove(ofType: eventType, withStorageId: nonExistentStorageId)
-        // If no error is thrown, the test passes
-    }
-
-    func testStoreCreatesDirectoryIfNeeded() async throws {
-        let event = ResetEvent() // Assume this is a new event type
-        try await eventStorageManager.store(event: event)
-
-        let eventTypeDirectory = await eventStorageManager.baseDirectory.appendingPathComponent(ResetEvent.key)
-        let directoryExists = FileManager.default.fileExists(atPath: eventTypeDirectory.path)
-        XCTAssertTrue(directoryExists, "Event storage should create the directory if it does not exist")
+    func testLoadEventsOfNonexistentType() async throws {
+        let loadedEvents = try await eventStorageManager.loadEvents(ofType: "NonexistentType")
+        XCTAssertTrue(loadedEvents.isEmpty, "Loading events for a nonexistent type should return an empty array")
     }
 
     func testLoadEventsWithCorruptDataContinuesLoading() async throws {
@@ -111,6 +97,30 @@ class EventStorageTest: UnitTest {
         let containsValidEvent = loadedEvents.contains { ($0 as? ProfileIdentifiedEvent)?.identifier == "testID" }
         XCTAssertTrue(containsValidEvent, "Valid events should be loaded even if corrupt data is present")
     }
+
+    // MARK: - Event Removal Tests
+
+    func testRemoveEvent() async throws {
+        let event = ProfileIdentifiedEvent(identifier: "testID")
+        try await eventStorageManager.store(event: event)
+
+        let eventFileURL = await eventStorageManager.baseDirectory
+            .appendingPathComponent(ProfileIdentifiedEvent.key)
+            .appendingPathComponent("\(event.storageId).json")
+        await eventStorageManager.remove(ofType: ProfileIdentifiedEvent.key, withStorageId: event.storageId)
+
+        let fileExists = FileManager.default.fileExists(atPath: eventFileURL.path)
+        XCTAssertFalse(fileExists, "Event file should not exist after removal")
+    }
+
+    func testRemoveEventWhenFileDoesNotExist() async throws {
+        let eventType = ProfileIdentifiedEvent.key
+        let nonExistentStorageId = UUID().uuidString
+        await eventStorageManager.remove(ofType: eventType, withStorageId: nonExistentStorageId)
+        // If no error is thrown, the test passes
+    }
+
+    // MARK: - Concurrency and Ordering Tests
 
     func testConcurrentEventStorageAndRetrieval() async throws {
         // Create multiple events
