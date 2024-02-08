c0fcc3e2b08e06f112266f57dd5b45ff6f344002
ami-aman <91549653+ami-aman@users.noreply.github.com>
2024-02-08 12:37:54 +0400

    sample env file writekey

--- Apps/APN-UIKit/BuildEnvironment.sample.swift
+++ Apps/APN-UIKit/BuildEnvironment.sample.swift
@@ -2,7 +2,6 @@ import Foundation
 
 class BuildEnvironment {
     enum CustomerIO {
-        static let siteId: String = "CUSTOMERIO_WORKSPACE_SITE_ID"
-        static let apiKey: String = "CUSTOMERIO_WORKSPACE_API_KEY"
+        static let writeKey: String = "CUSTOMERIO_CDP_WRITE_KEY"
     }
 }
