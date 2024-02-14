3bd6268bcc8ec1ee7ef50262157c27f3dc47517b
Shahroz Khan <shrozali@gmail.com>
2024-02-14 16:59:36 +0500

    reverted and shifted approach

--- Sources/Common/Util/Log.swift
+++ Sources/Common/Util/Log.swift
@@ -51,73 +51,28 @@ public enum CioLogLevel: String, CaseIterable {
     #endif
 }
 
-// log messages to console.
-// sourcery: InjectRegister = "Logger"
-public class ConsoleLogger: Logger {
-    // allows filtering in Console mac app
-    public static let logSubsystem = "io.customer.sdk"
-    public static let logCategory = "CIO"
-
-    private let sdkConfig: SdkConfig
-
-    private var abbreviatedSiteId: String {
-        sdkConfig.siteId.getFirstNCharacters(5)
-    }
-
-    private var minLogLevel: CioLogLevel {
-        sdkConfig.logLevel
-    }
-
-    init(sdkConfig: SdkConfig) {
-        self.sdkConfig = sdkConfig
-    }
-
-    private func printMessage(_ message: String, _ level: CioLogLevel) {
-        if !minLogLevel.shouldLog(level) { return }
-
-        let messageToPrint = "(siteid:\(abbreviatedSiteId)) \(message)"
-
-        ConsoleLogger.logMessageToConsole(messageToPrint, level: level)
-    }
-
-    public func debug(_ message: String) {
-        printMessage(message, .debug)
-    }
-
-    public func info(_ message: String) {
-        printMessage("ℹ️ \(message)", .info)
-    }
-
-    public func error(_ message: String) {
-        printMessage("🛑 \(message)", .error)
-    }
-
-    public static func logMessageToConsole(_ message: String, level: CioLogLevel) {
-        #if canImport(os)
-        // Unified logging for Swift. https://www.avanderlee.com/workflow/oslog-unified-logging/
-        // This means we can view logs in xcode console + Console app.
-        if #available(iOS 14, *) {
-            let logger = os.Logger(subsystem: self.logSubsystem, category: self.logCategory)
-            logger.log(level: level.osLogLevel, "\(message, privacy: .public)")
-        } else {
-            let logger = OSLog(subsystem: logSubsystem, category: logCategory)
-            os_log("%{public}@", log: logger, type: level.osLogLevel, message)
-        }
-        #else
-        // At this time, Linux cannot use `os.log` or `OSLog`. Instead, use: https://github.com/apple/swift-log/
-        // As we don't officially support Linux at this time, no need to add a dependency to the project.
-        // therefore, we are not logging if can't import os.log
-        #endif
+public extension DIGraph {
+    var logger: Logger {
+        DIGraphShared.shared.logger
     }
 }
 
+// log messages to console.
 // sourcery: InjectRegisterShared = "Logger"
-public class SharedConsoleLogger: Logger {
+public class ConsoleLogger: Logger {
+    public var logLevel: CioLogLevel = .error
+
+    public func setLogLevel(_ level: CioLogLevel) {
+        logLevel = level
+    }
+
     // allows filtering in Console mac app
     public static let logSubsystem = "io.customer.sdk"
     public static let logCategory = "CIO"
 
     private func printMessage(_ message: String, _ level: CioLogLevel) {
+        if !logLevel.shouldLog(level) { return }
+
         ConsoleLogger.logMessageToConsole(message, level: level)
     }
 
