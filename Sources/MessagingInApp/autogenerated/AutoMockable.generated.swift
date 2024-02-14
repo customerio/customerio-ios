0045c781361e730b7177c0254de3b6e553c8e2b1
Shahroz Khan <shrozali@gmail.com>
2024-02-14 17:41:08 +0500

    enable logging for gist

--- Sources/MessagingInApp/autogenerated/AutoMockable.generated.swift
+++ Sources/MessagingInApp/autogenerated/AutoMockable.generated.swift
@@ -277,21 +277,21 @@ class InAppProviderMock: InAppProvider, Mock {
     }
 
     /// The arguments from the *last* time the function was called.
-    private(set) var initializeReceivedArguments: (siteId: String, region: Region, delegate: GistDelegate)?
+    private(set) var initializeReceivedArguments: (siteId: String, region: Region, delegate: GistDelegate, enableLogging: Bool)?
     /// Arguments from *all* of the times that the function was called.
-    private(set) var initializeReceivedInvocations: [(siteId: String, region: Region, delegate: GistDelegate)] = []
+    private(set) var initializeReceivedInvocations: [(siteId: String, region: Region, delegate: GistDelegate, enableLogging: Bool)] = []
     /**
      Set closure to get called when function gets called. Great way to test logic or return a value for the function.
      */
-    var initializeClosure: ((String, Region, GistDelegate) -> Void)?
+    var initializeClosure: ((String, Region, GistDelegate, Bool) -> Void)?
 
-    /// Mocked function for `initialize(siteId: String, region: Region, delegate: GistDelegate)`. Your opportunity to return a mocked value and check result of mock in test code.
-    func initialize(siteId: String, region: Region, delegate: GistDelegate) {
+    /// Mocked function for `initialize(siteId: String, region: Region, delegate: GistDelegate, enableLogging: Bool)`. Your opportunity to return a mocked value and check result of mock in test code.
+    func initialize(siteId: String, region: Region, delegate: GistDelegate, enableLogging: Bool) {
         mockCalled = true
         initializeCallsCount += 1
-        initializeReceivedArguments = (siteId: siteId, region: region, delegate: delegate)
-        initializeReceivedInvocations.append((siteId: siteId, region: region, delegate: delegate))
-        initializeClosure?(siteId, region, delegate)
+        initializeReceivedArguments = (siteId: siteId, region: region, delegate: delegate, enableLogging: enableLogging)
+        initializeReceivedInvocations.append((siteId: siteId, region: region, delegate: delegate, enableLogging: enableLogging))
+        initializeClosure?(siteId, region, delegate, enableLogging)
     }
 
     // MARK: - setProfileIdentifier
