f870ea50e2465e4cffeaa12940b4db787ed6405c
Rehan <mrehan.official@gmail.com>
2024-02-20 15:33:23 +0500

    updated cdp config

--- Sources/DataPipeline/DataPipelineConfigOptions.swift
+++ Sources/DataPipeline/DataPipelineConfigOptions.swift
@@ -2,62 +2,27 @@ import CioInternalCommon
 import Foundation
 import Segment
 
-/**
- Configuration options for the Customer.io Data Pipeline module.
-
- Example use case:
- ```
- // create a new instance
- let dataPipelineConfig = DataPipelineConfigOptions.Factory.create(writeKey: "WRITE_KEY")
- // now, you can modify it
- dataPipelineConfig.apiHost = "https..."
- dataPipelineConfig.autoTrackScreenViews = false
- ```
- */
+/// Configuration options for the Customer.io Data Pipeline module
 public struct DataPipelineConfigOptions {
-    // Used to create new instance of DataPipelineConfigOptions when the DataPipeline module is configured.
-    // Each property of the DataPipelineConfigOptions object can be modified by the user.
-    public enum Factory {
-        public static func create(writeKey: String) -> DataPipelineConfigOptions {
-            DataPipelineConfigOptions(writeKey: writeKey)
-        }
-
-        public static func create(sdkConfig: SdkConfig) -> DataPipelineConfigOptions {
-            let writeKey = "\(sdkConfig.siteId):\(sdkConfig.apiKey)"
-            var result = DataPipelineConfigOptions(writeKey: writeKey)
-            result.autoTrackDeviceAttributes = sdkConfig.autoTrackDeviceAttributes
-            result.flushAt = sdkConfig.backgroundQueueMinNumberOfTasks
-            result.flushInterval = sdkConfig.backgroundQueueSecondsDelay
-            return result
-        }
-    }
-
-    private static let defaultAPIHost = "cdp.customer.io/v1"
-    private static let defaultCDNHost = "cdp.customer.io/v1"
-
-    init(writeKey: String) {
-        self.writeKey = writeKey
-    }
-
     /// Server key
     public let writeKey: String
 
     /// Host settings
-    public var apiHost: String = Self.defaultAPIHost
-    public var cdnHost: String = Self.defaultCDNHost
+    public let apiHost: String
+    public let cdnHost: String
 
     /// Dispatching configurations
-    public var flushAt: Int = 20
-    public var flushInterval: Seconds = 30
+    public let flushAt: Int
+    public let flushInterval: Seconds
 
     /// Segment configurations
-    public var autoAddCustomerIODestination: Bool = true
-    public var defaultSettings: Settings?
-    public var flushPolicies: [FlushPolicy] = [CountBasedFlushPolicy(), IntervalBasedFlushPolicy()]
-    public var flushQueue: DispatchQueue = .init(label: "com.segment.operatingModeQueue", qos: .utility)
-    public var operatingMode: OperatingMode = .asynchronous
-    public var trackApplicationLifecycleEvents: Bool = true
+    public let autoAddCustomerIODestination: Bool
+    public let defaultSettings: Settings?
+    public let flushPolicies: [FlushPolicy]
+    public let flushQueue: DispatchQueue
+    public let operatingMode: OperatingMode
+    public let trackApplicationLifecycleEvents: Bool
 
     /// Configuration options for users to easily add available plugins
-    public var autoTrackDeviceAttributes: Bool = true
+    public let autoTrackDeviceAttributes: Bool
 }
