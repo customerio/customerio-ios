32c878c3f01d753a188b6827a70c8b5225496a2c
Levi Bostian <levi@customer.io>
2024-02-21 14:08:19 -0600

    fix: all sdk modules can only be initialized once
    Closes: https://linear.app/customerio/issue/MBL-129/sdk-modules-can-only-be-initialized-1-time
    
    All SDK modules have a top-level `ModuleName.initialize()` function. We have a SDK requirement that this function is only executed one time and ignore all future calls.
    
    This change brings a few improvements to this requirement:
    1. Move implementation into base class, `ModuleTopLevelObject`, to encapsulate the logic and allow re-use by all SDK modules.
    2. Adds automated tests for `ModuleTopLevelObjectTest` super class to assert that our code only allows initializing modules one time. Note: These tests were originally written in `MessagingPushTest` and were moved to new `ModuleTopLevelObjectTest` class.
    
    commit-id:63854a04

--- Tests/MessagingPush/MessagingPushTest.swift
+++ Tests/MessagingPush/MessagingPushTest.swift
@@ -5,38 +5,20 @@ import SharedTests
 import XCTest
 
 class MessagingPushTest: IntegrationTest {
+    override func initializeSDKComponents() -> MessagingPushInstance? {
+        // We want to manually initialize the module in test functions. So, override this function to disable automatic module initialization.
+        nil
+    }
+
     private let automaticPushClickHandlingMock = AutomaticPushClickHandlingMock()
 
     override func setUp() {
-        setupTest()
+        super.setUp()
+
+        DIGraphShared.shared.override(value: automaticPushClickHandlingMock, forType: AutomaticPushClickHandling.self)
     }
 
     // MARK: initialize
-
-    func test_initialize_expectOnlyAbleToInitializeOnce_expectInitializeThreadSafe() {
-        // Run test multiple times to ensure thread safety. To try and catch a race condition, if one will exist.
-        // I do not suggest running test < 100 times. When bugs existed because of not being thread safe, the test may have to run 50 times until it fails.
-        runTest(numberOfTimes: 100) {
-            let expectAllThreadsToComplete = expectation(description: "All threads should complete")
-            expectAllThreadsToComplete.expectedFulfillmentCount = 2
-
-            runOnBackground {
-                MessagingPush.initialize()
-
-                expectAllThreadsToComplete.fulfill()
-            }
-
-            runOnBackground {
-                MessagingPush.initialize()
-
-                expectAllThreadsToComplete.fulfill()
-            }
-
-            waitForExpectations(1) // test may take up to 1 second to finish because it is running so many times. CI server is a less powerful machine and this test is flaky when we set wait() for < 1 second.
-
-            XCTAssertEqual(automaticPushClickHandlingMock.startCallsCount, 1)
-        }
-    }
 
     func test_initialize_givenDefaultModuleConfigOptions_expectStartAutoPushClickHandling() {
         MessagingPush.initialize()
@@ -45,20 +27,10 @@ class MessagingPushTest: IntegrationTest {
     }
 
     func test_initialize_givenCustomerDisabledAutoPushClickHandling_expectDoNotEnableFeature() {
-        setupTest { config in
-            config.autoTrackPushEvents = false
+        MessagingPush.initialize {
+            $0.autoTrackPushEvents = false
         }
 
-        MessagingPush.initialize()
-
         XCTAssertFalse(automaticPushClickHandlingMock.startCalled)
-    }
-}
-
-extension MessagingPushTest {
-    func setupTest(modifyModuleConfig: ((inout MessagingPushConfigOptions) -> Void)? = nil) {
-        super.setUp(modifyModuleConfig: modifyModuleConfig)
-
-        DIGraphShared.shared.override(value: automaticPushClickHandlingMock, forType: AutomaticPushClickHandling.self)
     }
 }
