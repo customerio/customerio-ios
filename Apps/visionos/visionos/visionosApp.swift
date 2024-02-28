f28ab70e04532521727e706e93f1b005646078a2
Ahmed Ali <eng.ahmed.ali.awad@gmail.com>
2024-02-29 01:51:18 +0400

    docs: Skeleton VisionOS app
    ## Context
    We are building a sample app for VisionOS to demonstrate the usage of CustomerIO SwiftSDK in VisionOS.
    
    ## In this PR
    In this PR we are introducing a new empty VisionOS app, with a single `Hello World` screen and the Swift Package Manger dependencies that will be used. Namely:
    - `swift-markdown-ui`: which will be used for imarkdown rendering.
    - `'splash`: dependency to create custom code syntax highlighting themes.
    - `customerio-swift`: local dependency on the CIO Swift SDK
    
    ## Test Plan:
    - Open visionos.xcproject in Xcode
    - Make sure you have VisionOS simulator installed and selected
    - CMD+R to run the app and see the `Hello World` screen

---
+++ Apps/visionos/visionos/visionosApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionosApp.swift
+//  visionos
+//
+//  Created by Ahmed Ali on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct visionosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
