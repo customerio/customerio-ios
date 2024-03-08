b9e8f272d61b2bc2d6329df31d67aa2111a03769
Ahmed Ali <eng.ahmed.ali.awad@gmail.com>
2024-03-08 17:05:39 +0400

    docs: [vision-on-sample-app][6/n] AppDelegate
    ## Context
    
    This is a PR in a series of PRs to build sample app that runs Identify and Track in VisionPro
    The app is built to be an interactive walk through guidance for developers who are integrating Customer.io Swift SDK in their VisioPro apps.
    For more context:
    
    - See the project [one pager](https://www.notion.so/custio/Spacial-Identify-and-Tracking-6587d848bbde494095c0585237326ed3?pvs=4)
    - See [Linear Issue](https://linear.app/customerio/issue/MBL-115/sample-app-for-visionpro)
    
    ## PR Summary
    This PR introduces fthe app delegate which will be used to initialize `CustomerIO`
    
    ## Test plan
    - Open `VisionOs.xcproject`
    - Hit CMD+R to run the project and make sure you have VisionOS simulator installed and selected
    
    [ghstack-poisoned]

---
+++ Apps/VisionOS/VisionOS/AppDelegate.swift
@@ -0,0 +1,39 @@
+import CioTracking
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        /*
+
+         How do I get the values for the siteId and apiKy?
+         1- Login or Signup in CustomerIO platform: https://fly.customer.io/
+         2- On the top right section click the
+         Settings Icon >> Workspace Settings >> API and webhook credentials
+         You will see list of one or more workspaces and each has
+         its own pair of siteId and apiKey, these are the ones
+         to use here.
+         */
+
+        let workspaceSettings = AppState.shared.workspaceSettings
+        if workspaceSettings.isSet() {
+            CustomerIO.initialize(
+                siteId: workspaceSettings.siteId,
+                apiKey: workspaceSettings.apiKey,
+                region: workspaceSettings.region
+            ) { config in
+                // Debug config just to make the demo
+                // easier. You can learn more about these configs
+                // here: https://customer.io/docs/sdk/ios/getting-started/#configuration-options
+                config.backgroundQueueMinNumberOfTasks = 1
+                config.backgroundQueueSecondsDelay = 0
+                config.logLevel = .debug
+            }
+        }
+
+        return true
+    }
+}
