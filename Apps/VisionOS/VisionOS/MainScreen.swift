70bcf45edf765366a1ca5a03080d1cd0d5304db7
Ahmed Ali <eng.ahmed.ali.awad@gmail.com>
2024-03-08 17:05:47 +0400

    docs: [vision-on-sample-app][8/n] App Intro View
    ## Context
    
    This is a PR in a series of PRs to build sample app that runs Identify and Track in VisionPro
    The app is built to be an interactive walk through guidance for developers who are integrating Customer.io Swift SDK in their VisioPro apps.
    For more context:
    
    - See the project [one pager](https://www.notion.so/custio/Spacial-Identify-and-Tracking-6587d848bbde494095c0585237326ed3?pvs=4)
    - See [Linear Issue](https://linear.app/customerio/issue/MBL-115/sample-app-for-visionpro)
    
    ## PR Summary
    This PR introduces the first screen in the app when the app runs for the first time.
    Intended to give the user an idea what to expect to see while using the app and quick call out on where they will find the actual `CustomerIO.[method]` calls if they all they care about is the code samples.
    
    Beside the regular code review, I would really appreciate any suggestion to improve the visual/texutal content.
    
    
    ## Test plan
    - Open `VisionOs.xcproject`
    - Hit CMD+R to run the project and make sure you have VisionOS simulator installed and selected
    - See the intro page similar to the one in the screenshot
    
    <img width="1663" alt="AppIntro" src="https://github.com/customerio/customerio-ios/assets/142905621/d90b9f1e-0ae4-4711-9949-6264a17fbacb">
    
    [ghstack-poisoned]

--- Apps/VisionOS/VisionOS/MainScreen.swift
+++ Apps/VisionOS/VisionOS/MainScreen.swift
@@ -1,24 +1,54 @@
+import CioTracking
+import MarkdownUI
 import SwiftUI
 
-struct MainScreen: View {
-    @ObservedObject var state: AppState = .shared
-    var body: some View {
-        VStack {
-            Text("Hello, world!")
-        }
-        .padding()
-        .environment(
-            \.openURL,
-            OpenURLAction { url in
-                guard let link = InlineNavigationLink(fromUrl: url) else {
-                    return .systemAction
-                }
-                state.navigationPath.append(link)
-                return .handled
-            }
-        )
+/**
+ This file is the main place for example CustomerIO usage
+ Its main objectives are:
+ - Basic logic to switch between different view
+ - Show all the calls to CustomerIO in one place for simplicity
+ */
+
+@ViewBuilder func view(forLink link: InlineNavigationLink) -> some View {
+    switch link {
+    case .sampleAppIntro:
+        SampleAppIntro()
+    default:
+        Text("Not implemented")
     }
 }
+
+// MARK: MainScreen View
+
+struct MainScreen: View {
+    @ObservedObject var state = AppState.shared
+
+    var body: some View {
+        CommonLayoutView {
+            NavigationStack(path: $state.navigationPath) {
+                view(forLink: state.navigationPath.first ?? .sampleAppIntro)
+                    .navigationDestination(for: InlineNavigationLink.self) { link in
+                        view(forLink: link)
+                            .navigationBarBackButtonHidden()
+                        Spacer()
+                    }
+                Spacer()
+            }
+            .environment(
+                \.openURL,
+                OpenURLAction { url in
+                    guard let link = InlineNavigationLink(fromUrl: url) else {
+                        return .systemAction
+                    }
+                    state.navigationPath = [link]
+                    return .handled
+                }
+            )
+        }
+    }
+}
+
+// MARK: Preview
 
 #Preview(windowStyle: .automatic) {
     MainScreen()
