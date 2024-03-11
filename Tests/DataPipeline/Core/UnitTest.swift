bc17f3b7717ea5435f3b674077610e70fd050c02
Rehan <mrehan.official@gmail.com>
2024-03-11 15:46:28 +0500

    minor fix

--- Tests/DataPipeline/Core/UnitTest.swift
+++ Tests/DataPipeline/Core/UnitTest.swift
@@ -75,7 +75,7 @@ open class UnitTest: SharedTests.UnitTestBase<CustomerIO> {
 
     override open func cleanupTestEnvironment() {
         super.cleanupTestEnvironment()
-        CustomerIO.resetTestEnvironment()
         Analytics.removeActiveWriteKey(dataPipelineConfigOptions.cdpApiKey)
+        CustomerIO.resetTestEnvironment()
     }
 }
