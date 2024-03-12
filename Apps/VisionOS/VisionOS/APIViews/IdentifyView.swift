b283d9ccb7173f836e395ebc61ee60ea9fe36b14
Ahmed Ali <eng.ahmed.ali.awad@gmail.com>
2024-03-12 07:53:39 +0400

    docs: [vision-os-sample-app][8/n] Identify view
    This is a PR in a series of PRs to build sample app that runs Identify and Track in VisionPro
    The app is built to be an interactive walk through guidance for developers who are integrating Customer.io Swift SDK in their VisioPro apps.
    For more context:
    
    - See the project [one pager](https://www.notion.so/custio/Spacial-Identify-and-Tracking-6587d848bbde494095c0585237326ed3?pvs=4)
    - See [Linear Issue](https://linear.app/customerio/issue/MBL-115/sample-app-for-visionpro)
    
    This PR introduces a the "Identify" example screen. It demonstrates the requirements of the API, allows the user to enter profile attributes to be send alongside the identify call.

---
+++ Apps/VisionOS/VisionOS/APIViews/IdentifyView.swift
@@ -0,0 +1,95 @@
+import MarkdownUI
+import Splash
+import SwiftUI
+
+private func identifyCodeSnippet(_ profile: Profile) -> String {
+    let propertiesStr = propertiesToTutorialString(
+        profile.traits,
+        linePrefix: "\t\t"
+    )
+
+    if !profile.userId.isEmpty, !profile.traits.isEmpty {
+        return
+            """
+            CustomerIO.shared.identify(
+                userId: "\(profile.userId)",
+                traits: \(propertiesStr)
+            )
+            """
+    } else if !profile.userId.isEmpty {
+        return
+            """
+            CustomerIO.shared.identify(
+                userId: "\(profile.userId)"
+            )
+            """
+    } else if !profile.traits.isEmpty {
+        return
+            """
+            CustomerIO.shared.identify(",
+                traits: \(propertiesStr)
+            )
+            """
+    }
+    return "// enter user id or add a trait"
+}
+
+struct IdentifyView: View {
+    @ObservedObject var state = AppState.shared
+    @State private var profile = AppState.shared.profile
+    @EnvironmentObject private var viewModel: ViewModel
+
+    let onSuccess: (Profile) -> Void
+
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack(alignment: .leading) {
+                Markdown {
+                    """
+                    Before you can track events, set devices attributes or update user traits, you must identify the user first.
+                    To identify a user, you must set a userId or traits or both.
+                    [Learn more](https://customer.io/docs/journeys/identifying-people/#identifiers).
+
+                    ```swift
+                    \(identifyCodeSnippet(profile))
+                    ```
+                    """
+                }
+
+                HStack(alignment: .bottom) {
+                    FloatingTitleTextField(title: "User ID", text: $profile.userId)
+                    Button("Generate UUID") {
+                        profile.userId = UUID().uuidString
+                    }
+                }
+
+                PropertiesInputView(
+                    terminology: .traits,
+                    showCodableHint: true,
+                    properties: $profile.traits
+                )
+
+                Divider()
+                HStack {
+                    Button("Identify") {
+                        profile.loggedIn = true
+                        if profile.userId.isEmpty, profile.traits.isEmpty {
+                            viewModel.errorMessage = "You must set an id or at least one trait"
+                            return
+                        }
+                        state.profile = profile
+                        onSuccess(state.profile)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AppState.shared.profile = Profile.empty()
+    return MainLayoutView(selectedExample: .constant(.identify)) {
+        IdentifyView { _ in
+        }
+    }
+}
