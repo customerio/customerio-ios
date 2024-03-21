e9ec6be1f3cfbe4fd3fadcf7f963fd90900d2685
Ahmed Ali <eng.ahmed.ali.awad@gmail.com>
2024-03-22 01:26:06 +0400

    docs: [vision-os-sample-app][5/n] AppDelegate
    This is a PR in a series of PRs to build sample app that runs Identify and Track in VisionPro
    The app is built to be an interactive walk through guidance for developers who are integrating Customer.io Swift SDK in their VisioPro apps.
    For more context:
    
    - See the project [one pager](https://www.notion.so/custio/Spacial-Identify-and-Tracking-6587d848bbde494095c0585237326ed3?pvs=4)
    - See [Linear Issue](https://linear.app/customerio/issue/MBL-115/sample-app-for-visionpro)
    
    This PR introduces fthe app delegate which will be used to initialize `CustomerIO`
    
    - Open `VisionOs.xcproject`
    - Hit CMD+R to run the project and make sure you have VisionOS simulator installed and selected
    
    commit-id:492ab528
    
    using data pipelines

--- Apps/VisionOS/VisionOS/VisionOSApp.swift
+++ Apps/VisionOS/VisionOS/VisionOSApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct VisionOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             MainScreen()
