a733afafe01fa7481e133573321ca2c681e23dd2
Shahroz Khan <shrozali@gmail.com>
2024-05-07 03:24:58 +0500

    removed unnecessary aliases

--- Sources/DataPipeline/Type/Aliases.swift
+++ Sources/DataPipeline/Type/Aliases.swift
@@ -3,31 +3,9 @@ import CioInternalCommon
 import Foundation
 
 /*
- Contains public aliases to expose public structures/protocols from the 'Segment' module
- to the 'DataPipeline' module. The goal is that customers do not need to import
- the 'Segment' module in their code.
+ Contains public aliases to expose public structures/protocols from the 'CioInternalCommon' module
+ to the 'DataPipeline' module.
  */
-
-public typealias Plugin = CioAnalytics.Plugin
-public typealias PluginType = CioAnalytics.PluginType
-public typealias EventPlugin = CioAnalytics.EventPlugin
-public typealias DestinationPlugin = CioAnalytics.DestinationPlugin
-public typealias UtilityPlugin = CioAnalytics.UtilityPlugin
-public typealias VersionedPlugin = CioAnalytics.VersionedPlugin
-
-public typealias Analytics = CioAnalytics.Analytics
-
-public typealias RawEvent = CioAnalytics.RawEvent
-public typealias TrackEvent = CioAnalytics.TrackEvent
-public typealias ScreenEvent = CioAnalytics.ScreenEvent
-public typealias AliasEvent = CioAnalytics.AliasEvent
-public typealias GroupEvent = CioAnalytics.GroupEvent
-public typealias IdentifyEvent = CioAnalytics.IdentifyEvent
-
-public typealias Settings = CioAnalytics.Settings
-public typealias FlushPolicy = CioAnalytics.FlushPolicy
-public typealias OperatingMode = CioAnalytics.OperatingMode
-
 public typealias Metric = CioInternalCommon.Metric
 public typealias CustomerIO = CioInternalCommon.CustomerIO
 public typealias CioLogLevel = CioInternalCommon.CioLogLevel
