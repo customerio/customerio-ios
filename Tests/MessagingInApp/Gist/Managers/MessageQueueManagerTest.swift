f519aebf8124d432a047ef00afcc2059a200e959
Levi Bostian <levi@customer.io>
2024-06-28 12:04:41 -0500

    fix bug where inline message list is not in a consistent order
    while testing in sample app, I was noticing that if you send 2 messages with identical priority to a device and you wait ~60 seconds with the app in the foreground, you will notice that the inline View will replace the currently shown message with another message without the user touching anything on the screen.

--- Tests/MessagingInApp/Gist/Managers/MessageQueueManagerTest.swift
+++ Tests/MessagingInApp/Gist/Managers/MessageQueueManagerTest.swift
@@ -64,6 +64,38 @@ class MessageQueueManagerTest: UnitTest {
         XCTAssertEqual(actualMessages, [givenMessage2, givenMessage1, givenMessage3])
     }
 
+    // We expect that given an identical set of Messages as input, no matter the order, the output order is always the same.
+    func test_getInlineMessages_givenMessagesWithSamePriority_expectConsistentReturnValue() {
+        let givenElementId = String.random
+
+        // Create multiple messages with the same priority.
+        let givenMessage1 = Message(elementId: givenElementId, priority: 1)
+        let givenMessage2 = Message(elementId: givenElementId, priority: 1)
+        let givenMessage3 = Message(elementId: givenElementId, priority: 1)
+
+        // Get the output 1 time to get a sample to compare against.
+        manager.localMessageStore = [
+            .random: givenMessage1,
+            .random: givenMessage2,
+            .random: givenMessage3
+        ]
+        let expectedOrder = manager.getInlineMessages(forElementId: givenElementId)
+
+        // Shuffle the input Messages Array and assert that the output is always the same.
+        for _ in 0 ..< 100 {
+            let shuffled = manager.localMessageStore.shuffled()
+
+            // Shuffle the order of the message store dictionary.
+            manager.localMessageStore = [:]
+            for (_, message) in shuffled.enumerated() {
+                manager.localMessageStore[.random] = message.value
+            }
+
+            // We expect that the output order is always the same
+            XCTAssertEqual(expectedOrder, manager.getInlineMessages(forElementId: givenElementId))
+        }
+    }
+
     // MARK: - processFetchedMessages
 
     func test_processFetchedMessages_givenEmptyMessages_expectNoProcessingDone() {
