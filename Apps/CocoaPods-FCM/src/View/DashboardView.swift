05b84beb697af7c581cd25ade9df46f552383b39
Levi Bostian <levi@customer.io>
2024-08-01 14:16:35 -0500

    dont need to set min height

--- Apps/CocoaPods-FCM/src/View/DashboardView.swift
+++ Apps/CocoaPods-FCM/src/View/DashboardView.swift
@@ -51,7 +51,7 @@ struct DashboardView: View {
                     Group {
                         InAppMessageViewRepresentable(elementId: "dashboard-announcement", onActionClick: { _, _, _ in
                             print("Custom callback received")
-                        }).frame(maxWidth: .infinity, minHeight: 0)
+                        }).frame(maxWidth: .infinity)
                         ColorButton("Send Random Event") {
                             switch Int.random(in: 0 ..< 3) {
                             case 0:
