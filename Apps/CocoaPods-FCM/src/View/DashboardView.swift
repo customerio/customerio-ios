e05c8d498b19b6953bcbd373e17485e5cf67c6e3
Levi Bostian <levi@customer.io>
2024-08-02 14:00:15 -0500

    bring back onaction click callback

--- Apps/CocoaPods-FCM/src/View/DashboardView.swift
+++ Apps/CocoaPods-FCM/src/View/DashboardView.swift
@@ -49,7 +49,9 @@ struct DashboardView: View {
                     }
                     Text("What would you like to test?")
                     Group {
-                        InlineInAppMessage(elementId: "dashboard-announcement").frame(maxWidth: .infinity)
+                        InlineInAppMessage(elementId: "dashboard-announcement", onActionClick: { _, _, _ in
+                            print("Custom callback received")
+                        }).frame(maxWidth: .infinity)
                         ColorButton("Send Random Event") {
                             switch Int.random(in: 0 ..< 3) {
                             case 0:
