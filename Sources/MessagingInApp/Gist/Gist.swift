5a61827238cec0109c963c50ca26698407848a13
Rehan <mrehan.official@gmail.com>
2024-08-19 17:58:13 +0500

    revert preloading

--- Sources/MessagingInApp/Gist/Gist.swift
+++ Sources/MessagingInApp/Gist/Gist.swift
@@ -23,6 +23,9 @@ public class Gist: GistDelegate {
         self.dataCenter = dataCenter
         Logger.instance.enabled = logging
         messageQueueManager.setup()
+
+        // Initialising Gist web with an empty message to fetch fonts and other assets.
+        _ = Gist.shared.getMessageView(Message(messageId: ""))
     }
 
     // MARK: User
