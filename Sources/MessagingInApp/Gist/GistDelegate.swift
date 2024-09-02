4e398a6dffbd7d6a5b5e0fa75551f32f56d805bb
Rehan <mrehan.official@gmail.com>
2024-09-02 22:31:29 +0500

    chore: moved gist delegate to separate class

--- Sources/MessagingInApp/Gist/GistDelegate.swift
+++ Sources/MessagingInApp/Gist/GistDelegate.swift
@@ -1,3 +1,5 @@
+import CioInternalCommon
+import Foundation
 import UIKit
 
 public protocol GistDelegate: AnyObject {
@@ -6,4 +8,72 @@ public protocol GistDelegate: AnyObject {
     func messageDismissed(message: Message)
     func messageError(message: Message)
     func action(message: Message, currentRoute: String, action: String, name: String)
+    func setEventListener(_ eventListener: InAppEventListener?)
+}
+
+// sourcery: InjectRegisterShared = "GistDelegate"
+// sourcery: InjectSingleton
+class GistDelegateImpl: GistDelegate {
+    private let logger: Logger
+    private let eventBusHandler: EventBusHandler
+
+    private var eventListener: InAppEventListener?
+
+    init(logger: Logger, eventBusHandler: EventBusHandler) {
+        self.logger = logger
+        self.eventBusHandler = eventBusHandler
+    }
+
+    func setEventListener(_ eventListener: InAppEventListener?) {
+        self.eventListener = eventListener
+    }
+
+    public func embedMessage(message: Message, elementId: String) {}
+
+    public func messageShown(message: Message) {
+        logger.debug("[InApp] Message shown: \(message.describeForLogs)")
+
+        if let deliveryId = getDeliveryId(from: message) {
+            eventBusHandler.postEvent(TrackInAppMetricEvent(deliveryID: deliveryId, event: InAppMetric.opened.rawValue))
+        }
+
+        eventListener?.messageShown(message: InAppMessage(gistMessage: message))
+    }
+
+    public func messageDismissed(message: Message) {
+        logger.debug("[InApp] Message dismissed: \(message.describeForLogs)")
+
+        eventListener?.messageDismissed(message: InAppMessage(gistMessage: message))
+    }
+
+    public func messageError(message: Message) {
+        logger.debug("[InApp] Message error: \(message.describeForLogs)")
+
+        eventListener?.errorWithMessage(message: InAppMessage(gistMessage: message))
+    }
+
+    public func action(message: Message, currentRoute: String, action: String, name: String) {
+        logger.debug("[InApp] Message action: \(action), \(message.describeForLogs)")
+
+        // a close action does not count as a clicked action.
+        if action != "gist://close" {
+            if let deliveryId = getDeliveryId(from: message) {
+                eventBusHandler.postEvent(TrackInAppMetricEvent(deliveryID: deliveryId, event: InAppMetric.clicked.rawValue, params: ["actionName": name, "actionValue": action]))
+            }
+        }
+
+        eventListener?.messageActionTaken(
+            message: InAppMessage(gistMessage: message),
+            actionValue: action,
+            actionName: name
+        )
+    }
+
+    private func getDeliveryId(from message: Message) -> String? {
+        guard let deliveryId = message.gistProperties.campaignId else {
+            return nil
+        }
+
+        return deliveryId
+    }
 }
--- Sources/MessagingInApp/MessagingInAppImplementation.swift
+++ Sources/MessagingInApp/MessagingInAppImplementation.swift
@@ -5,114 +5,62 @@ class MessagingInAppImplementation: MessagingInAppInstance {
     private let moduleConfig: MessagingInAppConfigOptions
 
     private let logger: Logger
-    private var inAppProvider: InAppProvider
-
-    private var eventListener: InAppEventListener?
+    private let inAppMessageManager: InAppMessageManager
+    private let gist: Gist
     private let threadUtil: ThreadUtil
-    let eventBusHandler: EventBusHandler
+    private let eventBusHandler: EventBusHandler
 
     init(diGraph: DIGraphShared, moduleConfig: MessagingInAppConfigOptions) {
         self.moduleConfig = moduleConfig
         self.logger = diGraph.logger
-        self.inAppProvider = diGraph.inAppProvider
+        self.inAppMessageManager = diGraph.inAppMessageManager
+        self.gist = Gist.shared
         self.threadUtil = diGraph.threadUtil
         self.eventBusHandler = diGraph.eventBusHandler
-        initialize()
+
+        subscribeToInAppMessageState()
     }
 
-    private func initialize() {
-        inAppProvider.initialize(
+    private func subscribeToInAppMessageState() {
+        inAppMessageManager.dispatch(action: .initialize(
             siteId: moduleConfig.siteId,
-            region: moduleConfig.region,
-            delegate: self,
-            enableLogging: logger.logLevel == .debug
-        )
+            dataCenter: moduleConfig.region.rawValue,
+            environment: GistEnvironment.production
+        )) {
+            self.subscribeToEventBus()
+        }
+    }
 
+    private func subscribeToEventBus() {
         // if identifier is already present, set the userToken again so in case if the customer was already identified and
         // module was added later on, we can notify gist about it.
         eventBusHandler.addObserver(ProfileIdentifiedEvent.self) { event in
             self.logger.debug("registering profile \(event.identifier) for in-app")
 
-            self.inAppProvider.setProfileIdentifier(event.identifier)
+            self.gist.setUserToken(event.identifier)
         }
 
         eventBusHandler.addObserver(ScreenViewedEvent.self) { event in
             self.logger.debug("setting route for in-app to \(event.name)")
 
             // Gist expects webview to be launched in main thread and changing route will trigger locally stored in-app messages for that route.
-            self.threadUtil.runMain {
-                self.inAppProvider.setRoute(event.name)
-            }
+            self.gist.setCurrentRoute(event.name)
         }
 
         eventBusHandler.addObserver(ResetEvent.self) { _ in
             self.logger.debug("removing profile for in-app")
 
             // Gist may dismiss a modal if it's displayed. Run on main thread to call UI code.
-            self.threadUtil.runMain {
-                self.inAppProvider.clearIdentify()
-            }
+            self.gist.resetState()
         }
     }
 
     func setEventListener(_ eventListener: InAppEventListener?) {
-        self.eventListener = eventListener
+        gist.setEventListener(eventListener)
     }
 
     // Dismiss in-app message
     func dismissMessage() {
-        inAppProvider.dismissMessage()
-    }
-}
-
-extension MessagingInAppImplementation: GistDelegate {
-    public func embedMessage(message: Message, elementId: String) {}
-
-    // Aka: message opened
-    public func messageShown(message: Message) {
-        logger.debug("in-app message opened. \(message.describeForLogs)")
-
-        eventListener?.messageShown(message: InAppMessage(gistMessage: message))
-
-        if let deliveryId = getDeliveryId(from: message) {
-            eventBusHandler.postEvent(TrackInAppMetricEvent(deliveryID: deliveryId, event: InAppMetric.opened.rawValue))
-        }
-    }
-
-    public func messageDismissed(message: Message) {
-        logger.debug("in-app message dismissed. \(message.describeForLogs)")
-
-        eventListener?.messageDismissed(message: InAppMessage(gistMessage: message))
-    }
-
-    public func messageError(message: Message) {
-        logger.error("error with in-app message. \(message.describeForLogs)")
-
-        eventListener?.errorWithMessage(message: InAppMessage(gistMessage: message))
-    }
-
-    public func action(message: Message, currentRoute: String, action: String, name: String) {
-        logger.debug("in-app action made. \(action), \(message.describeForLogs)")
-
-        // a close action does not count as a clicked action.
-        if action != "gist://close" {
-            if let deliveryId = getDeliveryId(from: message) {
-                eventBusHandler.postEvent(TrackInAppMetricEvent(deliveryID: deliveryId, event: InAppMetric.clicked.rawValue, params: ["actionName": name, "actionValue": action]))
-            }
-        }
-
-        eventListener?.messageActionTaken(
-            message: InAppMessage(gistMessage: message),
-            actionValue: action,
-            actionName: name
-        )
-    }
-
-    private func getDeliveryId(from message: Message) -> String? {
-        guard let deliveryId = message.gistProperties.campaignId else {
-            return nil
-        }
-
-        return deliveryId
+        gist.dismissMessage()
     }
 }
