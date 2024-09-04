40a4cb81bc3b76f19bb043aa18a0fb1e78309eb1
Rehan <mrehan.official@gmail.com>
2024-09-05 02:54:54 +0500

    removed unused method

--- Sources/Common/Util/ThreadUtil.swift
+++ Sources/Common/Util/ThreadUtil.swift
@@ -4,17 +4,12 @@ import Foundation
 public protocol ThreadUtil {
     func runBackground(_ block: @escaping () -> Void)
     func runMain(_ block: @escaping () -> Void)
-    func runMainAfterDelay(deadline: DispatchTime, _ block: @escaping () -> Void)
 }
 
 // sourcery: InjectRegisterShared = "ThreadUtil"
 public class CioThreadUtil: ThreadUtil {
     public func runMain(_ block: @escaping () -> Void) {
         DispatchQueue.main.async(execute: block)
-    }
-
-    public func runMainAfterDelay(deadline: DispatchTime, _ block: @escaping () -> Void) {
-        DispatchQueue.main.asyncAfter(deadline: deadline, execute: block)
     }
 
     public func runBackground(_ block: @escaping () -> Void) {
