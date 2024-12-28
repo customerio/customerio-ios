64dd1c05d1162370b51bfa2b18f6388ead013f26
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2024-12-28 17:05:25 +0400

    Use overlay color for in-app message background

---
+++ Sources/MessagingInApp/Gist/Utilities/UIColor+Hex.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+extension UIColor {
+    static func fromHex(color: String?) -> UIColor? {
+        guard var hexColor = color else { return nil }
+        if hexColor.hasPrefix("#") {
+            hexColor.removeFirst()
+        }
+
+        let scanner = Scanner(string: hexColor)
+
+        var rgbValue: UInt64 = 0
+        guard scanner.scanHexInt64(&rgbValue) else {
+            return nil
+        }
+
+        var red, green, blue, alpha: UInt64
+        switch hexColor.count {
+        case 6:
+            red = (rgbValue >> 16)
+            green = (rgbValue >> 8 & 0xFF)
+            blue = (rgbValue & 0xFF)
+            alpha = 255
+        case 8:
+            red = (rgbValue >> 16)
+            green = (rgbValue >> 8 & 0xFF)
+            blue = (rgbValue & 0xFF)
+            alpha = rgbValue >> 24
+        default:
+            return nil
+        }
+
+        return UIColor(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: CGFloat(alpha) / 255)
+    }
+}
