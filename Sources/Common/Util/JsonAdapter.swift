1e652060f1be1787d09fbb8436c8cd21369a1784
Rehan <mrehan.official@gmail.com>
2025-01-20 12:04:23 +0500

    pr suggestions

--- Sources/Common/Util/JsonAdapter.swift
+++ Sources/Common/Util/JsonAdapter.swift
@@ -59,42 +59,17 @@ public class JsonAdapter {
         self.log = log
     }
 
-    // MARK: Dictionary to JSON
-
     public func fromDictionary<T: Decodable>(_ dictionary: [AnyHashable: Any]) -> T? {
-        commonDecodeDictionary(dictionary)
-    }
-
-    public func fromDictionary<T: Decodable>(_ dictionary: [[AnyHashable: Any]]) -> T? {
-        commonDecodeDictionary(dictionary)
-    }
-
-    public func fromDictionary(_ dictionary: [AnyHashable: Any]) -> Data? {
-        commonDecodeDictionaryToData(dictionary)
-    }
-
-    public func fromDictionary(_ dictionary: [[AnyHashable: Any]]) -> Data? {
-        commonDecodeDictionaryToData(dictionary)
-    }
-
-    private func commonDecodeDictionary<T: Decodable>(_ dictionary: Any) -> T? {
-        guard let data = commonDecodeDictionaryToData(dictionary) else {
-            return nil
-        }
-
-        return fromJson(data)
-    }
-
-    private func commonDecodeDictionaryToData(_ dictionary: Any) -> Data? {
         do {
-            return try JSONSerialization.data(withJSONObject: dictionary)
+            let jsonData = try JSONSerialization.data(withJSONObject: dictionary)
+
+            return fromJson(jsonData)
         } catch {
-            log.error("\(error.localizedDescription), object: \(dictionary)")
+            log.error("\(error.localizedDescription), dictionary: \(dictionary)")
         }
+
         return nil
     }
-
-    // MARK: JSON to Dictionary
 
     public func toDictionary<T: Encodable>(_ obj: T) -> [AnyHashable: Any]? {
         guard let data = toJson(obj) else {
