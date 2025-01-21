e6fd143f92d72c21e4de8d893f57d4e1504de0c3
Muhammad Rehan <mrehan.official@gmail.com>
2025-01-21 13:16:10 +0500

    chore: merge non-conflicting changes from inline in-app feature branch (#851)

---
+++ Sources/MessagingInApp/Views/GistInlineInAppMessageView.swift
@@ -0,0 +1,34 @@
+import CioInternalCommon
+import Foundation
+import UIKit
+
+// Event listener for interactions and state changes to an inline inapp message that's rendered.
+public protocol GistInlineMessageUIViewDelegate: AnyObject {
+    // After a message is finished rendering and the size may have changed.
+    func onMessageRendered(width: CGFloat, height: CGFloat)
+    // If there is no longer any messages to be shown.
+    func onNoMessageToDisplay()
+    // A custom action button is pressed.
+    // @return if you handled the action yourself.
+    func onInlineButtonAction(message: Message, currentRoute: String, action: String, name: String) -> Bool
+    // If the inline View is going to show a different message. Example: When a user clicks "show another message" action buttons.
+    func willChangeMessage(newTemplateId: String, onComplete: @escaping () -> Void)
+}
+
+// swiftlint:disable todo
+// TODO: Reimplement this class from feature branch.
+// swiftlint:enable todo
+public class GistInlineMessageUIView: UIView {
+    public var elementId: String?
+    public weak var delegate: GistInlineMessageUIViewDelegate?
+
+    public init(elementId: String) {
+        super.init(frame: .zero)
+        self.elementId = elementId
+    }
+
+    // This is called when the View is created from a Storyboard.
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
