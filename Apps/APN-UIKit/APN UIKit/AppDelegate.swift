16a97e74fe620f7bc9fba3ca5b05b9f5f09768a4
uros-mil <uros.mil@gmail.com>
2025-05-14 15:52:02 +0200

    Small change

--- Apps/APN-UIKit/APN UIKit/AppDelegate.swift
+++ Apps/APN-UIKit/APN UIKit/AppDelegate.swift
@@ -12,7 +12,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
         initializeCioAndInAppListeners()
 
         /*
