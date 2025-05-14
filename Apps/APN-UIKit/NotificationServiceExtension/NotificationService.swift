e901d8b4fa8ab1f3eecfc12f1c81349c8899aa04
uros-mil <uros.mil@gmail.com>
2025-05-14 14:31:50 +0200

    Commented out custom implementation of `UNUserNotificationCenterDelegate` and added explanation when to un-comment it. NotificationService clean-up

--- Apps/APN-UIKit/NotificationServiceExtension/NotificationService.swift
+++ Apps/APN-UIKit/NotificationServiceExtension/NotificationService.swift
@@ -2,9 +2,6 @@ import CioMessagingPushAPN
 import UserNotifications
 
 class NotificationService: UNNotificationServiceExtension {
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
-
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         MessagingPushAPN.initializeForExtension(
             withConfig: MessagingPushConfigBuilder(cdpApiKey: BuildEnvironment.CustomerIO.cdpApiKey)
