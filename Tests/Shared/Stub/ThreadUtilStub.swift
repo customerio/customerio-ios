9871dd0bb95307c3229416d7796d6851182fc25f
Shahroz Khan <shrozali@gmail.com>
2025-05-20 23:58:06 +0500

    added tests for gist delegate implementation

--- Tests/Shared/Stub/ThreadUtilStub.swift
+++ Tests/Shared/Stub/ThreadUtilStub.swift
@@ -2,11 +2,25 @@ import CioInternalCommon
 import Foundation
 
 public class ThreadUtilStub: ThreadUtil {
+    public var runMainCalled = false
+    public var runBackgroundCalled = false
+    
+    public init() {
+        // Public initializer
+    }
+    
     public func runMain(_ block: @escaping () -> Void) {
+        runMainCalled = true
         block()
     }
 
     public func runBackground(_ block: @escaping () -> Void) {
+        runBackgroundCalled = true
         block()
+    }
+    
+    public func reset() {
+        runMainCalled = false
+        runBackgroundCalled = false
     }
 }
