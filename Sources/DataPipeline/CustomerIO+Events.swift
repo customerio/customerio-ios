e1b753f567af6e29017f302caea39e77caffd2ef
Mahmoud Elmorabea <mahmoud.elmorabea@gmail.com>
2025-08-12 18:47:21 +0300

    chore: Deprecating unnecessary tracking APIs (#945)

--- Sources/DataPipeline/CustomerIO+Events.swift
+++ Sources/DataPipeline/CustomerIO+Events.swift
@@ -11,10 +11,12 @@ public extension CustomerIO {
     /// Associate a user with their unique ID and record traits about them.
     /// - Parameters:
     ///   - traits: A dictionary of traits you know about the user. Things like: email, name, plan, etc.
+    @available(*, deprecated, message: "Use 'setProfileAttributes(_:)' with [String: Any] instead. Support for Codable traits will be removed in a future version.")
     func identify<T: Codable>(traits: T) {
         DataPipeline.shared.analytics.identify(traits: traits)
     }
 
+    @available(*, deprecated, message: "Use 'screen(title:properties:)' with [String: Any] properties parameter instead. Support for Codable properties will be removed in a future version.")
     func screen<P: Codable>(title: String, category: String? = nil, properties: P?) {
         DataPipeline.shared.analytics.screen(title: title, category: category, properties: properties)
     }
