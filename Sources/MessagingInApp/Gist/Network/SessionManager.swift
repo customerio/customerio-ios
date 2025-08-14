4b06a93d6f5f64c184e88f56d1b7eb2fde0ab034
Bernard Gatt <bernard@customer.io>
2025-08-14 13:34:34 +0200

    Fixes linting issue, Swift classes are internal by default.

--- Sources/MessagingInApp/Gist/Network/SessionManager.swift
+++ Sources/MessagingInApp/Gist/Network/SessionManager.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// Manages session-level identifiers that persist for the lifetime of the app launch
-internal class SessionManager {
+class SessionManager {
     static let shared = SessionManager()
     let sessionId: String
 
