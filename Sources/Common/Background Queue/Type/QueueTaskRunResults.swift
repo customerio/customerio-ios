6c2b61eb2302b51e086c0e826e36ce88e5bf70bb
Holly Schilling <hollyschilling-cio@users.noreply.github>
2025-12-27 22:49:16 -0600

    Removed AutoLenses and all autogenerated code.

--- Sources/Common/Background Queue/Type/QueueTaskRunResults.swift
+++ Sources/Common/Background Queue/Type/QueueTaskRunResults.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-public struct QueueTaskRunResults: Codable, AutoLenses, Equatable {
-    let totalRuns: Int
+public struct QueueTaskRunResults: Codable, Equatable {
+    public var totalRuns: Int
 
     enum CodingKeys: String, CodingKey {
         case totalRuns = "total_runs"
