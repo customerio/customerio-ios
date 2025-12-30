850eb74b56c51007150427c5d090563f458999c6
Holly Schilling <hollyschilling-cio@users.noreply.github>
2025-12-30 10:16:10 -0600

    Rewrote AtomicTests to prevent them from failing intermittently while migrating them to use SwiftTesting. Removed additional references to AutoLenses.

--- Sources/Common/autogenerated/Autogenerated.swift
+++ Sources/Common/autogenerated/Autogenerated.swift
@@ -4,8 +4,3 @@ import Foundation
  View `AutoMockable.generated.swift` file to learn how to use this protocol.
  */
 public protocol AutoMockable {}
-
-/**
- View `AutoLenses.generated.swift` file to learn how to use this protocol.
- */
-public protocol AutoLenses {}
