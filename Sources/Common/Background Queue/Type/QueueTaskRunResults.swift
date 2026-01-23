6afcb2f9d8f1f525f3b7fe1762700f129e6db139
Holly Schilling <hollyschilling-cio@users.noreply.github>
2026-01-22 21:33:34 -0600

    Expanded mutability and visibility of QueueTaskRunResults as suggested by CursorBot.

--- Sources/Common/Background Queue/Type/QueueTaskRunResults.swift
+++ Sources/Common/Background Queue/Type/QueueTaskRunResults.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct QueueTaskRunResults: Codable, Equatable {
-    let totalRuns: Int
+    public var totalRuns: Int
 
     enum CodingKeys: String, CodingKey {
         case totalRuns = "total_runs"
