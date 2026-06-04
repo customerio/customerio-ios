28379536b28ad803e0eeb73e38af1e928936f77d
Holly Schilling <hollyschilling-cio@users.noreply.github>
2026-06-04 09:31:21 -0500

    Restored empty extension for UserDefaults in the APNs Demo App.

--- Apps/APN-UIKit/APN UIKit/autogenerated/DIDependencies.swift
+++ Apps/APN-UIKit/APN UIKit/autogenerated/DIDependencies.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+// sourcery: InjectRegisterShared = "UserDefaults"
+// sourcery: InjectCustomShared
+// swiftformat:disable:next emptyExtensions
+extension UserDefaults {}
+
 extension DIGraphShared {
     var customUserDefaults: UserDefaults {
         UserDefaults.standard
